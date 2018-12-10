8e21cec3a2a898d4af6491d0576bee879c1f050f
Björn Schmidtke <bs@pa1nd.de>
2018-12-10 20:05:14 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,115 +1,43 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import { login } from './redux/actions'
-import logo from './assets/logo.png'
-import './App.css'
-import platform from './env.js'
+import Login from './containers/Login'
+import Dashboard from './containers/Dashboard'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      user: null,
-      password: null
-    }
-  }
+let App = props => (
+  <div className="App">
+    <Switch>
+      <PrivateRoute
+        authed={props.username}
+        exact
+        path="/"
+        component={Dashboard}
+      />
+      <Route path="/login" component={Login} />
+    </Switch>
+  </div>
+)
 
-  handleSubmit() {
-    const url =
-      platform.oauth +
-      `/token?username=${this.state.username}&password=${
-        this.state.password
-      }&grant_type=password`
-
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-        Authorization: 'Basic YmFyQ2xpZW50SWRQYXNzd29yZDpzZWNyZXQ='
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: { from: props.location } }}
+          />
+        )
       }
-    })
-      .then(response => response.json())
-      .then(responseJson => {
-        this.saveToLocalStorage(responseJson)
-      })
-  }
-
-  saveToLocalStorage = data => {
-    const { access_token, user } = data
-    console.log(access_token, user.username)
-    this.props.setLogin(user.username, access_token)
-    // localStorage.setItem('user', JSON.stringify(user))
-    // localStorage.setItem('access_token', access_token)
-    // session.save({ access_token, user })
-  }
-
-  onChange(name, e) {
-    this.setState({
-      [name]: e.target.value
-    })
-  }
-
-  onKeyPress = e => {
-    if (e.which === 13) {
-      this.handleSubmit()
-    }
-  }
-
-  render() {
-    return (
-      <div className="form-signin text-center">
-        <img className="mb-4" src={logo} alt="" width="72" height="72" />
-        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label htmlFor="username" className="sr-only">
-          Username
-        </label>
-        <input
-          type="text"
-          id="username"
-          className="form-control"
-          placeholder="Username"
-          required
-          autoFocus
-          onChange={e => this.onChange('username', e)}
-          onKeyPress={this.onKeyPress}
-        />
-        <label htmlFor="inputPassword" className="sr-only">
-          Password
-        </label>
-        <input
-          type="password"
-          id="inputPassword"
-          className="form-control"
-          placeholder="Password"
-          required
-          onChange={e => this.onChange('password', e)}
-          onKeyPress={this.onKeyPress}
-        />
-        <button
-          className="btn btn-lg btn-primary btn-block"
-          type="submit"
-          onClick={e => this.handleSubmit(e)}
-        >
-          Sign in
-        </button>
-        <p className="mt-5 mb-3 text-muted">
-          Povered by Stoplight &copy; 2017 - {new Date().getFullYear()}
-        </p>
-      </div>
-    )
-  }
+    />
+  )
 }
 
 const mapStateToProps = state => ({
-  stateObject: state
+  state: state
 })
-const mapDispatchToProps = dispatch => ({
-  setLogin: (username, token) => {
-    dispatch(login(username, token))
-  }
-})
-export default (App = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App))
+
+export default (App = connect(mapStateToProps)(App))
