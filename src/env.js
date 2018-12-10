52a4578dae3ee9d1a3a8f5cbab9358e2714253da
Björn Schmidtke <bs@pa1nd.de>
2018-12-10 18:56:54 +0100

    add and initialize redux. store token.

---
+++ src/env.js
@@ -0,0 +1,6 @@
+const platform = {
+  api: 'https://testing.backend.povertystoplight.org/api/v1',
+  oauth: 'https://testing.backend.povertystoplight.org/oauth'
+}
+
+export default platform
---
+++ src/redux/actions.js
@@ -0,0 +1,5 @@
+export const login = (username, token) => ({
+  type: 'LOGIN',
+  username,
+  token
+})
