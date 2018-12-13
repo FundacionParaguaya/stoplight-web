473e777d28b58aa465db4f13a5a3f02de1666954
Jacqueline Yang <justinvyang@gmail.com>
2018-12-13 03:11:16 -0800

    add final-form and form for socio economic questions

--- src/containers/Surveys.js
+++ src/containers/Surveys.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { loadSurveys } from '../redux/actions'
+import { FormSpy, Form, Field } from 'react-final-form'
 
 class Surveys extends Component {
   componentDidMount() {
@@ -17,10 +18,62 @@ class Surveys extends Component {
       : []
     return (
       <div>
-        <ul>
-          {data &&
-            data.map((question, i) => <li key={i}>{question.questionText}</li>)}
-        </ul>
+        <Form
+          onSubmit={(values, form) => {
+            console.log('submitted: ', values)
+          }}
+          initialValues={{}}
+          render={({
+            handleSubmit,
+            submitting,
+            pristine,
+            values,
+            form,
+            invalid
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <FormSpy
+                subscription={{ values: true }}
+                component={({ values }) => {
+                  if (values) {
+                    console.log('formspy stuff: ', values)
+                  }
+                  return ''
+                }}
+              />
+              {data &&
+                data.map(question => (
+                  <div key={question.questionText}>
+                    <Field name={question.codeName}>
+                      {({ input, meta }) => (
+                        <div className="form-group">
+                          <label>{question.questionText}</label>
+                          <input
+                            type="text"
+                            {...input}
+                            className="form-control"
+                            placeholder=""
+                          />
+                          {meta.touched && meta.error && (
+                            <span>{meta.error}</span>
+                          )}
+                        </div>
+                      )}
+                    </Field>
+                  </div>
+                ))}
+              <div style={{ paddingTop: 20 }}>
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-sm btn-block"
+                  disabled={pristine || invalid}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          )}
+        />
       </div>
     )
   }
