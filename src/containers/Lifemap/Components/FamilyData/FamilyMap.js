d91b31d16cbae9b693338023b31abc43cb10ad1a
Bryan Giger <bryan@gigerbyt.es>
2019-01-17 16:02:44 +0100

    add country, postcode & address fields to map component

--- src/containers/Lifemap/Components/FamilyData/FamilyMap.js
+++ src/containers/Lifemap/Components/FamilyData/FamilyMap.js
@@ -1,77 +1,154 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Gmaps, Marker } from 'react-gmaps'
+import { Form, Field } from 'react-final-form'
 import { addSurveyData, addSurveyDataWhole } from '../../../../redux/actions'
-import {Gmaps, Marker, InfoWindow} from 'react-gmaps';
 
-
-
-const params = {v: '3.exp', key: 'AIzaSyAOJGqHfbWY_u7XhRnLi7EbVjdK-osBgAM'};
+const params = { v: '3.exp', key: 'AIzaSyAOJGqHfbWY_u7XhRnLi7EbVjdK-osBgAM' }
 
 class FamilyMap extends Component {
-  constructor(){
+  constructor() {
     super()
-    this.state = { // set paraguay HQ
+    this.state = {
+      // set paraguay HQ
       lat: -25.3092612,
       lng: -57.5872545
     }
     this.getLocation()
-
   }
 
   async getLocation() {
     if (navigator.geolocation) {
-      await navigator.geolocation.getCurrentPosition((position) => {
+      await navigator.geolocation.getCurrentPosition(position => {
         this.setState({
           lat: position.coords.latitude,
           lng: position.coords.longitude
         })
-      });
+      })
     } else {
       return
     }
+    // call redux action to save location!
   }
 
   onMapCreated(map) {
     map.setOptions({
       disableDefaultUI: true
-    });
+    })
   }
 
   onDragEnd(e) {
-    console.log('onDragEnd', e);
+    console.log('onDragEnd', e)
+    console.log(e)
+    //update the state
   }
 
   onCloseClick() {
-    console.log('onCloseClick');
+    console.log('onCloseClick')
   }
 
   onClick(e) {
-    console.log('onClick', e);
+    console.log('onClick', e)
   }
 
   render() {
-
     return (
-      <Gmaps
-        width={'800px'}
-        height={'600px'}
-        lat={this.state.lat}
-        lng={this.state.lng}
-        zoom={12}
-        loadingMessage={'Please wait while the map is loading.'}
-        params={params}
-        onMapCreated={this.onMapCreated}>
-        <Marker
+      <div style={{ marginTop: 50 }}>
+        <Gmaps
+          width={'800px'}
+          height={'600px'}
           lat={this.state.lat}
           lng={this.state.lng}
-          draggable={true}
-          onDragEnd={this.onDragEnd} />
-      </Gmaps>
-    );
+          zoom={12}
+          loadingMessage={'Please wait while the map is loading.'}
+          params={params}
+          onMapCreated={this.onMapCreated}
+        >
+          <Marker
+            lat={this.state.lat}
+            lng={this.state.lng}
+            draggable={true}
+            onDragEnd={this.onDragEnd}
+          />
+        </Gmaps>
+        <Form
+          onSubmit={(values, form) => {
+            // values.firstParticipant = true
+            // values.birthDate = this.state.date
+            // this.props.addSurveyDataWhole(this.props.draftId, 'family_data', {
+            //   familyMembersList: [values]
+            // })
+            // this.props.setName(values['firstName'])
+            this.props.nextStep()
+          }}
+          initialValues={{}}
+          render={({
+            handleSubmit,
+            submitting,
+            pristine,
+            values,
+            focm,
+            invalid
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label> country </label>
+                <Field
+                  name="country"
+                  component="select"
+                  className="custom-select"
+                >
+                  <option value="">Country</option>
+                </Field>
+              </div>
+              <Field name="postCode">
+                {({ input, meta }) => (
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      {...input}
+                      className="form-control"
+                      placeholder="Post Code"
+                    />
+                    {meta.touched && meta.error && <span>{meta.error}</span>}
+                  </div>
+                )}
+              </Field>
+              <Field name="address">
+                {({ input, meta }) => (
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      {...input}
+                      className="form-control"
+                      placeholder="Address"
+                    />
+                    {meta.touched && meta.error && <span>{meta.error}</span>}
+                  </div>
+                )}
+              </Field>
+              <div style={{ paddingTop: 20 }}>
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-lg"
+                  disabled={pristine || invalid}
+                >
+                  Submit
+                </button>
+                <button
+                  className="btn btn-primary btn-lg"
+                  onClick={() => this.props.previousStep()}
+                >
+                  Go Back
+                </button>
+              </div>
+            </form>
+          )}
+        />
+      </div>
+    )
   }
-
-};
-
+}
 
 const mapDispatchToProps = {
   addSurveyData,
