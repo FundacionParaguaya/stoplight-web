823a84a70dbf9693102e2322f6a8d7fe99f34635
Bryan Giger <bryan@gigerbyt.es>
2019-01-19 17:36:34 +0100

    add skip question button & set stoplight cards to one row

--- src/containers/Lifemap/Components/StopLight/StopLightPresentational.js
+++ src/containers/Lifemap/Components/StopLight/StopLightPresentational.js
@@ -8,25 +8,25 @@ const StopLightPresentational = ({
   previousStep,
   parentPreviousStep
 }) => {
+  console.log(data)
   return (
     <div>
       <div>
         <p>{data.questionText}</p>
         <p>{`${index + 1}/${total}`}</p>
       </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <div>
+      <div  className="row">
+      <div className="col-lg-4">
         <button
           type="submit"
           onClick={() => nextStep(data.stoplightColors[0], data.codeName)}
-          styles={{ maxWidth: '10%' }}
         >
           <div
             className="card text-white bg-success mb-3"
             styles={{ maxWidth: '10%' }}
           >
-            <div className="card-header" styles={{ maxWidth: '25%' }}>
-              {data.stoplightColors[0].description}
-            </div>
+
             <div className="card-body">
               <img
                 src={data.stoplightColors[0].url}
@@ -34,8 +34,13 @@ const StopLightPresentational = ({
                 styles={{ maxWidth: '25%' }}
               />
             </div>
+            <div className="card-footer" styles={{ maxWidth: '25%' }}>
+              {data.stoplightColors[0].description}
+            </div>
           </div>
         </button>
+        </div>
+        <div class="col-lg-4">
         <button
           type="submit"
           onClick={() => nextStep(data.stoplightColors[1], data.codeName)}
@@ -44,18 +49,20 @@ const StopLightPresentational = ({
             className="card text-white bg-danger mb-3"
             styles={{ maxWidth: '10%' }}
           >
-            <div className="card-header" styles={{ maxWidth: '25%' }}>
-              {data.stoplightColors[2].description}
-            </div>
             <div className="card-body">
               <img
                 src={data.stoplightColors[1].url}
                 alt={data.stoplightColors[1].description}
                 styles={{ maxWidth: '25%' }}
               />
+              <div className="card-footer" styles={{ maxWidth: '25%' }}>
+                {data.stoplightColors[0].description}
+              </div>
             </div>
           </div>
         </button>
+        </div>
+        <div class="col-lg-4">
         <button
           type="submit"
           onClick={() => nextStep(data.stoplightColors[2], data.codeName)}
@@ -64,18 +71,31 @@ const StopLightPresentational = ({
             className="card text-white bg-warning mb-3"
             styles={{ maxWidth: '10%' }}
           >
-            <div className="card-header">
-              {data.stoplightColors[2].description}
-            </div>
+
             <div className="card-body">
               <img
                 src={data.stoplightColors[2].url}
                 alt={data.stoplightColors[2].description}
                 styles={{ maxWidth: '25%' }}
               />
+              <div className="card-footer" styles={{ maxWidth: '25%' }}>
+                {data.stoplightColors[0].description}
+              </div>
             </div>
           </div>
         </button>
+        </div>
+        </div>
+
+        <hr/>
+        <br/>
+        <p>
+        {!data.required &&
+          <button type="submit"
+            onClick={() => nextStep(0, data.codeName)}>
+            Skip this question</button>
+        }
+        </p>
 
         <button
           className="btn btn-primary btn-lg"
