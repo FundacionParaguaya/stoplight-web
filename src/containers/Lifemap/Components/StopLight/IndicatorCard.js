6fd6ba0552bb5dc1e161d76da487715ebf6168b5
Bryan Giger <bryan@gigerbyt.es>
2019-01-19 20:44:46 +0100

    reformat indicatorCard

--- src/containers/Lifemap/Components/StopLight/IndicatorCard.js
+++ src/containers/Lifemap/Components/StopLight/IndicatorCard.js
@@ -1,27 +1,12 @@
 import React from 'react'
 
-const IndicatorCard  = ({
-  url,
-  description,
-  cardClass
-}) => {
-  return(
-    <div
-
-      className={cardClass}
-      styles={{ maxWidth: '10%' }}
-    >
-
+const IndicatorCard = ({ url, description, cardClass }) => {
+  return (
+    <div className={cardClass}>
       <div className="card-body">
-        <img
-          src={url}
-          alt=""
-          style={{maxWidth:"100%"}}
-        />
+        <img src={url} alt="" style={{ maxWidth: '100%' }} />
       </div>
-      <div className="card-footer">
-        {description}
-      </div>
+      <div className="card-footer">{description}</div>
     </div>
   )
 }
