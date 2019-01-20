45a993585fb2134f4e30d2ae8c04c834d1808946
Bryan Giger <bryan@gigerbyt.es>
2019-01-20 22:25:15 +0100

    add image assets

--- src/containers/Lifemap/Components/TermsPrivacy/TermsPrivacyPresentational.js
+++ src/containers/Lifemap/Components/TermsPrivacy/TermsPrivacyPresentational.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import Checkbox from '../../../../assets/Checkbox.png'
 const TermsPrivacyPresentational = ({ data, header, nextStep }) => {
   return (
     <div style={{ marginTop: 50 }}>
       <h2>{header}</h2>
       <hr />
+      <div className="text-center">
+        <img src={Checkbox} alt="chekbox" />
+      </div>
       <div>
-        <p className="lead">{data.title}</p>
-        <hr className="my-4" />
+        <div className="text-center">
+          <h3>{data.title}</h3>
+          <hr className="my-4" />
+        </div>
         <p
           dangerouslySetInnerHTML={{
             __html: data.text.split('\\n').join('<br/>')
