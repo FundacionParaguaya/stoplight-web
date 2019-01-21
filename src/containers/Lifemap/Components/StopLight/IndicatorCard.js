19c80d2017228c8b3d56e1302a060f0b6e36dbd2
Bryan Giger <bryan@gigerbyt.es>
2019-01-21 12:12:33 +0100

    load images all at once with spinner to show loading

--- src/containers/Lifemap/Components/StopLight/IndicatorCard.js
+++ src/containers/Lifemap/Components/StopLight/IndicatorCard.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import Spinner from '../../../../components/Spinner'
 
-const IndicatorCard = ({ url, description, cardClass }) => {
+const IndicatorCard = ({
+  url,
+  description,
+  cardClass,
+  cardImageLoaded,
+  imagesLoaded
+}) => {
+  let imgStyle =
+    imagesLoaded !== 3
+      ? { display: 'none', maxWidth: '100%' }
+      : { maxWidth: '100%' }
   return (
-    <div className={cardClass}>
-      <div className="card-body">
-        <img src={url} alt="" style={{ maxWidth: '100%' }} />
+    <div>
+      <div>{imagesLoaded !== 3 ? <Spinner /> : <div />}</div>
+      <div className={cardClass} style={imgStyle}>
+        <div className="card-body">
+          <img src={url} alt="" onLoad={() => cardImageLoaded()} />
+        </div>
+        <div className="card-footer">{description}</div>
       </div>
-      <div className="card-footer">{description}</div>
     </div>
   )
 }
