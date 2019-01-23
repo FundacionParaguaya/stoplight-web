89099609730afc16e6d9d749b1bbfeb4df245613
Bryan Giger <bryan@gigerbyt.es>
2019-01-24 00:15:13 +0100

    Remove nav + sidebar

--- src/App.js
+++ src/App.js
@@ -45,27 +45,6 @@ class App extends Component {
             </div>
           ) : (
             <div>
-              <nav className="navbar navbar-dark bg-primary fixed-top flex-md-nowrap p-0 shadow">
-                <Link
-                  to="/"
-                  className="navbar-brand col-md-3 col-lg-2 mr-0"
-                  href="#"
-                >
-                  <img src={logo} alt="" className="nav-logo" />
-                  <span className="brand">Stoplight</span>
-                </Link>
-
-                <ul className="navbar-nav px-3">
-                  <li className="nav-item text-nowrap">
-                    <button
-                      className="nav-link btn btn-link"
-                      onClick={this.props.logout}
-                    >
-                      <Power className="feather" /> Sign out
-                    </button>
-                  </li>
-                </ul>
-              </nav>
 
               <div className="container-fluid">
                 <div className="row">
