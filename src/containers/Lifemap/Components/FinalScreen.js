5e00510f53ef0567af579736d25346b594e370fd
Björn Schmidtke <bs@pa1nd.de>
2019-02-01 09:04:54 -0300

    fix margins

--- src/containers/Lifemap/Components/FinalScreen.js
+++ src/containers/Lifemap/Components/FinalScreen.js
@@ -6,26 +6,38 @@ import AppNavbar from '../../../components/AppNavbar'
 
 class FinalScreen extends Component {
   render() {
-
-    const { t, submitDraft, submitError} = this.props
+    const { t, submitDraft, submitError } = this.props
     return (
       <div>
-      <AppNavbar
-        text={t('views.yourLifeMap')}
-        showBack={true}
-        backHandler={this.props.parentPreviousStep}
-      />
-      <div className="text-center">
-        <h3> {t('views.lifemap.great')}</h3>
-        <h3> {t('views.lifemap.youHaveCompletedTheLifemap')}</h3>
-        <div className="text-center">
+        <AppNavbar
+          text={t('views.yourLifeMap')}
+          showBack={true}
+          backHandler={this.props.parentPreviousStep}
+        />
+        <div className="text-center" style={{ padding: '100px 20px' }}>
+          <h2 style={{ paddingBottom: '50px' }}>
+            {t('views.lifemap.great')}
+            <br />
+            {t('views.lifemap.youHaveCompletedTheLifemap')}
+          </h2>
           <img src={lifemap_complete_image} alt="lifemap_complete_image" />
+          {submitError && (
+            <div style={{ color: 'red', margin: '1em' }}>
+              {' '}
+              There was an error in submitting. The data is still in your
+              browser. Please do not close the browser and contact Poverty
+              Stoplight as soon as possible.
+            </div>
+          )}
         </div>
-        {submitError && (<div style={{color: 'red', margin: '1em'}}> There was an error in submitting. The data is still in your browser. Please do not close the browser and contact Poverty Stoplight as soon as possible.</div>)}
-          <button className="btn btn-primary btn-lg btn-block" onClick={() => submitDraft()}>
+        <div style={{ paddingTop: 20 }}>
+          <button
+            className="btn btn-primary btn-lg btn-block"
+            onClick={() => submitDraft()}
+          >
             {t('general.close')}
           </button>
-      </div>
+        </div>
       </div>
     )
   }
