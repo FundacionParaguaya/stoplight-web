f173cd6b13dcd2c5d3cacc8e1bf0a27ff0a9808e
Bryan Giger <bryan@gigerbyt.es>
2019-02-14 23:25:22 +0100

    set card height to 400px height

--- src/containers/Lifemap/Components/StopLight/IndicatorCard.js
+++ src/containers/Lifemap/Components/StopLight/IndicatorCard.js
@@ -10,16 +10,16 @@ const IndicatorCard = ({
   let imgStyle =
     imagesLoaded !== 3
       ? { display: 'none', maxWidth: '100%' }
-      : { maxWidth: '100%' }
+      : { maxWidth: '100%', height: '400px'}
   return (
     <div>
       <div>{imagesLoaded !== 3 ? <Spinner /> : <div />}</div>
-      <div className={cardClass} style={imgStyle}>
-        <div className="card-body">
+      <div className={cardClass} style={imgStyle} >
+        <div className="card-body" >
           <img src={url} className="img-fluid" alt="" onLoad={() => cardImageLoaded()} />
-        </div>
-        <div className="card-footer">{description}</div>
+          <div style={{marginTop: '1em'}}><p >{description}</p></div>
       </div>
+    </div>
     </div>
   )
 }
