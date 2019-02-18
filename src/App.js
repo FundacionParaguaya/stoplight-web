aa5285d4a0c7a3728bd6af99bd4ea0e760b91db5
carloscarvallo <carloscarvallo87@gmail.com>
2019-02-18 11:21:53 -0300

    minor improvement on indentation

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch
-} from 'react-router-dom'
-import { connect } from 'react-redux'
+} from "react-router-dom";
+import { connect } from "react-redux";
 
-import { logout, login } from './redux/actions'
-import Surveys from './containers/Surveys'
-import Lifemap from './containers/Lifemap'
+import { logout, login } from "./redux/actions";
+import Surveys from "./containers/Surveys";
+import Lifemap from "./containers/Lifemap";
 
-import Dots from './components/Dots'
+import Dots from "./components/Dots";
 
 function getParams(location) {
-  const searchParams = new URLSearchParams(location.search)
-  let env = document.referrer.split('.')[0].split('//')[1] || 'testing'
-  console.log(env)
+  const searchParams = new URLSearchParams(location.search);
+  let env = document.referrer.split(".")[0].split("//")[1] || "testing";
+  console.log(env);
   switch (env) {
-    case 'testing':
-      env = 'test'
-      break
-    case 'demo':
-      env = 'demo'
-      break
-    case 'platform':
-      env = 'prod'
-      break
+    case "testing":
+      env = "test";
+      break;
+    case "demo":
+      env = "demo";
+      break;
+    case "platform":
+      env = "prod";
+      break;
     default:
   }
   return {
-    sid: searchParams.get('sid') || '',
-    username: searchParams.get('username') || '',
+    sid: searchParams.get("sid") || "",
+    username: searchParams.get("username") || "",
     env: env
-  }
+  };
 }
 
 /**
@@ -42,48 +42,54 @@ function getParams(location) {
  */
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       urlParams: getParams(window.location)
-    }
+    };
 
-    if (this.state.urlParams.sid !== '') {
-      this.props.setLogin(this.state.urlParams.sid, this.state.urlParams.env)
+    if (this.state.urlParams.sid !== "") {
+      this.props.setLogin(this.state.urlParams.sid, this.state.urlParams.env);
     }
   }
   render() {
-    if(this.props.state.user.token){
+    if (this.props.state.user.token) {
       return (
         <Router>
-        <div>
-        <Dots />
-        <div className="main-card card">
-        <Switch>
-        <Route exact path="/surveys" component={Surveys} />
-        <Route exact path="/lifemap" component={Lifemap} />
-        </Switch>
-        </div>
-        </div>
+          <div>
+            <Dots />
+            <div className="main-card card">
+              <Switch>
+                <Route exact path="/surveys" component={Surveys} />
+                <Route exact path="/lifemap" component={Lifemap} />
+              </Switch>
+            </div>
+          </div>
         </Router>
-      )
-    } else { return <div> <Dots/></div>}
-
+      );
+    } else {
+      return (
+        <div>
+          {" "}
+          <Dots />
+        </div>
+      );
+    }
   }
 }
 
 const mapStateToProps = state => ({
   state: state
-})
+});
 const mapDispatchToProps = dispatch => ({
   logout: () => {
-    dispatch(logout())
+    dispatch(logout());
   },
   setLogin: (token, env) => {
-    dispatch(login(token, env))
+    dispatch(login(token, env));
   }
-})
+});
 
 export default (App = connect(
   mapStateToProps,
   mapDispatchToProps
-)(App))
+)(App));
