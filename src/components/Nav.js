73cc76108d94c5a3167ff3e6fc8fc6281a20f564
Bryan Giger <bryan@gigerbyt.es>
2019-02-28 03:23:00 +0100

    set nav style for lg, md, sm add logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import icon_stoplight from '../assets/icon_stoplight.png'
 
 const Nav = props => {
   return (
     <div >
-      <nav className="navbar navbar-expand-lg ">
+      <nav className="navbar navbar-expand-lg navbar-expand-md navbar-expand-sm ">
+      <ul className="navbar-nav povstop-logo-nav">
+      <li className="nav-item " >
+        <a className="nav-link" href="#"><img src={icon_stoplight} height="42px"/></a>
+      </li>
+      </ul>
       <ul className="navbar-nav">
-      <li className="nav-item active" >
+      <li className="nav-item " >
         <a className="nav-link" href="#" >Surveys </a>
       </li>
       <li className="nav-item">
