1b382144f62d9445db3c2ad6ad20a471f1425332
Dan Atanasov <danmanm@gmail.com>
2019-03-19 16:10:27 +0200

    Start app anew

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist'
-import store from './redux/store'
-import { I18nextProvider } from 'react-i18next'
-import i18n from './i18n'
-
-import './styles/index.css'
 import App from './App'
 
-let persistor = persistStore(store)
-
-ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
