d83335e2c2330e65b5465e5d4858ee1c62861bf8
Dan Atanasov <danmanm@gmail.com>
2019-03-19 16:10:27 +0200

    Start app anew

--- src/App.js
+++ src/App.js
@@ -1,105 +1,25 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { connect } from "react-redux";
+import React, { Component } from 'react'
 
-import { logout, login } from "./redux/actions";
-import i18n from "./i18n";
-import Surveys from "./containers/Surveys";
-import Lifemap from "./containers/Lifemap";
-
-import Nav from "./components/Nav";
-import Dots from "./components/Dots";
-
-function getParams(location) {
-  const searchParams = new URLSearchParams(location.search);
-  let env = document.referrer.split(".")[0].split("//")[1] || "testing";
-  console.log(env);
-  switch (env) {
-    case "testing":
-      env = "test";
-      break;
-    case "demo":
-      env = "demo";
-      break;
-    case "platform":
-      env = "prod";
-      break;
-    default:
-  }
-  return {
-    sid: searchParams.get("sid") || "",
-    username: searchParams.get("username") || "",
-    env: env
-  };
-}
-
-/**
- * This is the Entry Pyont
- * @param {object} state - redux state that contains user information
- */
 class App extends Component {
-  constructor(props) {
-    super(props);
-    let url = new URL(window.location);
-    this.state = {
-      urlParams: getParams(window.location)
-    };
-
-    if (this.state.urlParams.sid !== "") {
-      this.props.setLogin(this.state.urlParams.sid, this.state.urlParams.env);
-    }
-
-    if (url.searchParams.get("lang")) {
-      let lang = url.searchParams.get("lang");
-      i18n.changeLanguage(lang);
-    }
-  }
   render() {
-    const {
-      state: {
-        user: { token }
-      }
-    } = this.props;
-    if (token) {
-      return (
-        <Router>
-          <div>
-            <Dots />
-            <div>
-              <Nav />
-            </div>
-            <div className="main-card card">
-              <Switch>
-                <Route exact path="/surveys" component={Surveys} />
-                <Route path="/lifemap" component={Lifemap} />
-              </Switch>
-            </div>
-          </div>
-        </Router>
-      );
-    } else {
-      return (
-        <div>
-          <Dots />
-        </div>
-      );
-    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
   }
 }
 
-const mapStateToProps = state => ({
-  state: state
-});
-const mapDispatchToProps = dispatch => ({
-  logout: () => {
-    dispatch(logout());
-  },
-  setLogin: (token, env) => {
-    dispatch(login(token, env));
-  }
-});
-
-export default (App = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App));
+export default App
