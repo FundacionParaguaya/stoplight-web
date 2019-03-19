7fd9a2625d4151369fcdbbbd84c869d4b4b3ed93
Dan Atanasov <danmanm@gmail.com>
2019-03-19 16:36:04 +0200

    Setup initial router

---
+++ src/screens/Lifemap.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return <h1>Lifemap</h1>
+  }
+}
+
+export default App
