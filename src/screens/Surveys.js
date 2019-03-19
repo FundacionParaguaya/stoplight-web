f3dbba6b8e9e80a0d339c56e54ffe1b4d615262e
Dan Atanasov <danmanm@gmail.com>
2019-03-19 16:36:04 +0200

    Setup initial router

---
+++ src/screens/Surveys.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return <h1>Surveys</h1>
+  }
+}
+
+export default App
