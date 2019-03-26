3e85b666a0d137a1a586883e8b33928f468a962c
Dan Atanasov <danmanm@gmail.com>
2019-03-26 16:29:42 +0200

    Connect terms and privacy with primary participant

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline' // provides css reset
+import { PersistGate } from 'redux-persist/integration/react'
 import Header from './Header'
-import Surveys from './screens/Surveys'
+import SurveysComponent from './screens/Surveys'
 import Lifemap from './screens/Lifemap'
-import store from './redux'
+import store, { persistor } from './redux'
 import defaultTheme from './theme'
 
 class App extends Component {
@@ -16,17 +17,19 @@ class App extends Component {
         <React.Fragment>
           <CssBaseline />
           <Provider store={store}>
-            <Router>
-              <div>
-                <Header />
-                <div style={{ marginTop: 60 }}>
-                  <Switch>
-                    <Route path="/surveys" component={Surveys} />
-                    <Route path="/lifemap" component={Lifemap} />
-                  </Switch>
+            <PersistGate persistor={persistor}>
+              <Router>
+                <div>
+                  <Header />
+                  <div style={{ marginTop: 60 }}>
+                    <Switch>
+                      <Route path="/surveys" component={SurveysComponent} />
+                      <Route path="/lifemap" component={Lifemap} />
+                    </Switch>
+                  </div>
                 </div>
-              </div>
-            </Router>
+              </Router>
+            </PersistGate>
           </Provider>
         </React.Fragment>
       </MuiThemeProvider>
--- src/screens/Terms.js
+++ src/screens/Terms.js
@@ -3,11 +3,18 @@ import { connect } from 'react-redux'
 import Button from '@material-ui/core/Button'
 
 export class Terms extends Component {
+  handleContinue = () => {
+    this.props.history.push(
+      this.props.location.pathname === '/lifemap/terms'
+        ? '/lifemap/privacy'
+        : '/lifemap/primary-participant'
+    )
+  }
   render() {
     return (
       <div>
         <h2>Terms</h2>
-        <Button variant="contained" fullWidth>
+        <Button variant="contained" fullWidth onClick={this.handleContinue}>
           Continue
         </Button>
       </div>
@@ -17,9 +24,7 @@ export class Terms extends Component {
 
 const mapStateToProps = ({ currentSurvey }) => ({ currentSurvey })
 
-const mapDispatchToProps = {}
-
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  {}
 )(Terms)
