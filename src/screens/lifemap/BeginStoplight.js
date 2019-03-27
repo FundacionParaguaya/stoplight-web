bfc8c343e3507d0bdec455b44d088ee3c6588432
Pavel Belyaev <zommerberg@abv.bg>
2019-03-27 22:13:19 +0200

    added reusable foler,BeginStoplight component and added translation to some components

--- src/screens/lifemap/BeginStoplight.js
+++ src/screens/lifemap/BeginStoplight.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Button from '@material-ui/core/Button'
-
+import { withStyles } from '@material-ui/core/styles'
+import { withTranslation } from 'react-i18next'
+import TopTitleContainer from './reusable/TopTitleContainer'
+import lifemap_begin_image from '../../assets/lifemap_begin_image.png'
 export class Begin extends Component {
   render() {
+    const { classes, t, currentSurvey } = this.props
+    let questions = currentSurvey.surveyStoplightQuestions.length
     return (
       <div>
-        <h2>Begin Lifemap</h2>
+        <TopTitleContainer title={t('views.yourLifeMap')} />
+        <div className={classes.BeginStopLightContainer}>
+          <h2 className={classes.StopLightTitleContainer}>
+            {t('views.lifemap.thisLifeMapHas').replace('%n', questions)}}
+          </h2>
+          <img
+            style={{ margin: '70px 0 100px 0' }}
+            src={lifemap_begin_image}
+            alt=""
+          />
+        </div>
         <Button
           variant="contained"
+          color="primary"
           fullWidth
           onClick={() => this.props.history.push('/lifemap/stoplight/0')}
+          style={{ color: 'white' }}
         >
-          Continue
+          {t('general.continue')}
         </Button>
       </div>
     )
@@ -21,7 +38,18 @@ export class Begin extends Component {
 
 const mapStateToProps = ({ currentSurvey }) => ({ currentSurvey })
 
-export default connect(
-  mapStateToProps,
-  {}
-)(Begin)
+const styles = {
+  BeginStopLightContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  StopLightTitleContainer: {
+    width: 460,
+    margin: '40px auto 0 auto',
+    textAlign: 'center'
+  }
+}
+export default withStyles(styles)(
+  connect(mapStateToProps)(withTranslation()(Begin))
+)
