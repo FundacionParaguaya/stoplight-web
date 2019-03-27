6a0813a2d066ed8685ed7f6d1dc530d88e3b688f
Dan Atanasov <danmanm@gmail.com>
2019-03-27 16:30:11 +0200

    Setup draft update

---
+++ src/i18n.js
@@ -0,0 +1,27 @@
+import i18n, { use } from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import en from './locales/en.json'
+import es from './locales/es.json'
+
+// the translations
+// (tip move them in a JSON file and import them)
+const resources = {
+  en: {
+    translation: en
+  },
+  es: {
+    translation: es
+  }
+}
+
+use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: 'en',
+
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  })
+
+export default i18n
--- src/screens/lifemap/PrimaryParticipant.js
+++ src/screens/lifemap/PrimaryParticipant.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { withTranslation } from 'react-i18next'
 import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 import uuid from 'uuid/v1'
 import { updateDraft } from '../../redux/actions'
 
@@ -34,6 +36,27 @@ export class PrimaryParticipant extends Component {
     this.props.history.push('/lifemap/location')
   }
 
+  updateDraft = (field, event) => {
+    const { currentDraft } = this.props
+
+    this.props.updateDraft({
+      ...currentDraft,
+      familyData: {
+        ...currentDraft.familyData,
+        familyMembersList: [
+          ...currentDraft.familyData.familyMembersList.slice(0, 0),
+          {
+            ...currentDraft.familyData.familyMembersList[0],
+            ...{
+              [field]: event.target.value
+            }
+          },
+          ...currentDraft.familyData.familyMembersList.slice(1)
+        ]
+      }
+    })
+  }
+
   componentDidMount() {
     // if there is no current draft in the store create a new one
     if (!this.props.currentDraft) {
@@ -42,11 +65,30 @@ export class PrimaryParticipant extends Component {
   }
 
   render() {
+    const { t } = this.props
+    const participant = this.props.currentDraft.familyData.familyMembersList[0]
+
+    console.log(this.props.currentSurvey.surveyConfig.documentType)
+
     return (
       <div>
-        <h2>Primary Participant</h2>
+        <h2>{t('views.primaryParticipant')}</h2>
+        <TextField
+          label={t('views.family.firstName')}
+          value={participant.firstName || ''}
+          fullWidth
+          onChange={e => this.updateDraft('firstName', e)}
+          margin="normal"
+        />
+        <TextField
+          label={t('views.family.lastName')}
+          value={participant.lastName || ''}
+          fullWidth
+          onChange={e => this.updateDraft('lastName', e)}
+          margin="normal"
+        />
         <Button variant="contained" fullWidth onClick={this.handleContinue}>
-          Continue
+          {t('general.continue')}
         </Button>
       </div>
     )
@@ -63,4 +105,4 @@ const mapDispatchToProps = { updateDraft }
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(PrimaryParticipant)
+)(withTranslation()(PrimaryParticipant))
