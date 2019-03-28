b7bc244303493ba8ef605fd43723c26c0021b71d
Dan Atanasov <danmanm@gmail.com>
2019-03-28 12:55:32 +0200

    Setup country select

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
 import TextField from '@material-ui/core/TextField'
+import { withStyles } from '@material-ui/core/styles'
 
 class Input extends Component {
   render() {
     return (
       <TextField
+        className={this.props.classes.input}
         label={this.props.label}
         value={this.props.value || ''}
         onChange={this.props.onChange}
         fullWidth
-        margin="normal"
       />
     )
   }
 }
 
-export default Input
+const styles = {
+  input: {
+    marginTop: 10,
+    marginBottom: 10
+  }
+}
+
+export default withStyles(styles)(Input)
--- src/components/Select.js
+++ src/components/Select.js
@@ -3,26 +3,45 @@ import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
 import InputLabel from '@material-ui/core/InputLabel'
 import FormControl from '@material-ui/core/FormControl'
+import { withStyles } from '@material-ui/core/styles'
+import countries from 'localized-countries'
 
-class Input extends Component {
+const countryList = countries(require('localized-countries/data/en')).array()
+
+console.log(countryList)
+
+class SelectInput extends Component {
   render() {
     return (
-      <FormControl fullWidth>
+      <FormControl className={this.props.classes.container} fullWidth>
         <InputLabel shrink>{this.props.label}</InputLabel>
         <Select
           value={this.props.value || ''}
           fullWidth
           onChange={this.props.onChange}
         >
-          {this.props.options.map(item => (
-            <MenuItem key={item.value} value={item.value}>
-              {item.text}
-            </MenuItem>
-          ))}
+          {!!this.props.country
+            ? countryList.map(country => (
+                <MenuItem key={country.code} value={country.code}>
+                  {country.label}
+                </MenuItem>
+              ))
+            : this.props.options.map(item => (
+                <MenuItem key={item.value} value={item.value}>
+                  {item.text}
+                </MenuItem>
+              ))}
         </Select>
       </FormControl>
     )
   }
 }
 
-export default Input
+const styles = {
+  container: {
+    marginTop: 10,
+    marginBottom: 10
+  }
+}
+
+export default withStyles(styles)(SelectInput)
