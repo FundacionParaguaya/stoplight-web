ef0e980479680edbdc75ed04cd8fd3341d751d1b
Dan Atanasov <danmanm@gmail.com>
2019-03-29 11:01:52 +0200

    Revert last commit

--- src/screens/lifemap/PrimaryParticipant.js
+++ src/screens/lifemap/PrimaryParticipant.js
@@ -9,6 +9,10 @@ import Input from '../../components/Input'
 import Select from '../../components/Select'
 
 export class PrimaryParticipant extends Component {
+  state = {
+    householdSizeArray: []
+  }
+
   createNewDraft() {
     const { currentSurvey } = this.props
 
@@ -41,12 +45,14 @@ export class PrimaryParticipant extends Component {
   updateDraft = (field, event) => {
     const { currentDraft } = this.props
 
+    // update only the first item of familyMembersList
+    //  which is the primary participant
     this.props.updateDraft({
       ...currentDraft,
       familyData: {
         ...currentDraft.familyData,
         familyMembersList: [
-          ...currentDraft.familyData.familyMembersList.slice(0),
+          ...currentDraft.familyData.familyMembersList.slice(0, 0),
           {
             ...currentDraft.familyData.familyMembersList[0],
             ...{
@@ -59,11 +65,48 @@ export class PrimaryParticipant extends Component {
     })
   }
 
+  updateFamilyMembersCount = event => {
+    const { currentDraft } = this.props
+
+    console.log(event.target.value)
+
+    this.props.updateDraft({
+      ...currentDraft,
+      familyData: {
+        ...currentDraft.familyData,
+        ...{ countFamilyMembers: event.target.value }
+      }
+    })
+  }
+
+  setHouseholdSizeArray() {
+    const { t } = this.props
+    const householdSizeArray = []
+
+    for (var i = 1; i <= 26; i++) {
+      householdSizeArray.push({
+        value: i === 26 ? -1 : i,
+        text:
+          i === 1
+            ? t('views.family.onlyPerson')
+            : i === 26
+            ? t('views.family.preferNotToSay')
+            : `${i}`
+      })
+    }
+
+    this.setState({
+      householdSizeArray
+    })
+  }
+
   componentDidMount() {
     // if there is no current draft in the store create a new one
     if (!this.props.currentDraft) {
       this.createNewDraft()
     }
+
+    this.setHouseholdSizeArray()
   }
 
   render() {
@@ -73,6 +116,8 @@ export class PrimaryParticipant extends Component {
     const participant = this.props.currentDraft
       ? this.props.currentDraft.familyData.familyMembersList[0]
       : {}
+
+    console.log(this.state.householdSizeArray)
 
     return (
       <div>
@@ -115,6 +160,17 @@ export class PrimaryParticipant extends Component {
           value={participant.birthCountry}
           onChange={e => this.updateDraft('birthCountry', e)}
           country
+        />
+
+        <Select
+          label={t('views.family.peopleLivingInThisHousehold')}
+          value={
+            this.props.currentDraft
+              ? this.props.currentDraft.familyData.countFamilyMembers
+              : null
+          }
+          onChange={this.updateFamilyMembersCount}
+          options={this.state.householdSizeArray}
         />
 
         <Input
