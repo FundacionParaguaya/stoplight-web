86232825014b9ea7a5bd1087b55495482de22db2
Pavel Belyaev <zommerberg@abv.bg>
2019-03-29 12:39:54 +0200

    added social economics questions and updated the location map

--- src/screens/lifemap/Economics.js
+++ src/screens/lifemap/Economics.js
@@ -5,7 +5,8 @@ import Button from '@material-ui/core/Button'
 import { withStyles } from '@material-ui/core/styles'
 import { withTranslation } from 'react-i18next'
 import TitleBar from '../../components/TitleBar'
-
+import Input from '../../components/Input'
+import Select from '../../components/Select'
 export class Economics extends Component {
   state = {
     questions: null,
@@ -36,7 +37,39 @@ export class Economics extends Component {
       topic: questions.forFamily[0].topic
     })
   }
+  updateDraft(codeName, answer) {
+    const { currentDraft } = this.props
+    let dataList = this.props.currentDraft.economicSurveyDataList
+    let update = false
+    //////////////// CHECK IF THE QUESTION IS ALREADY IN THE DATA LIST and if it is the set update to true and edit the answer
+    dataList.forEach(e => {
+      if (e.key === codeName) {
+        update = true
+        e.value = answer.target.value
+      }
+    })
 
+    ///////////if the question is in the data list then update the question
+    if (update) {
+      let economicSurveyDataList = dataList
+      this.props.updateDraft({
+        ...currentDraft,
+        economicSurveyDataList
+      })
+    } else {
+      //////////// add the question to the data list if it doesnt exist
+      this.props.updateDraft({
+        ...currentDraft,
+        economicSurveyDataList: [
+          ...currentDraft.economicSurveyDataList,
+          {
+            key: codeName,
+            value: answer.target.value
+          }
+        ]
+      })
+    }
+  }
   componentDidMount() {
     this.setCurrentScreen()
   }
@@ -49,16 +82,46 @@ export class Economics extends Component {
 
   render() {
     const { questions, topic } = this.state
-    const { t } = this.props
+    const { t, currentDraft } = this.props
+    console.log(questions)
+    console.log(currentDraft)
     return (
       <div>
         <TitleBar title={topic} />
         {/* List of questions for current topic */}
 
         {questions &&
-          questions.forFamily.map(question => (
-            <p key={question.codeName}>{question.questionText}</p>
-          ))}
+          questions.forFamily.map(q => {
+            let selectValue
+            currentDraft.economicSurveyDataList.forEach(e => {
+              if (e.key === q.codeName) {
+                selectValue = e.value
+              }
+            })
+            if (q.answerType === 'select') {
+              return (
+                <div key={q.codeName}>
+                  <Select
+                    label={q.questionText}
+                    value={selectValue}
+                    onChange={e => this.updateDraft(q.codeName, e)}
+                    options={q.options}
+                  />
+                </div>
+              )
+            } else {
+              return (
+                <div key={q.codeName}>
+                  <Input
+                    label={q.questionText}
+                    value={selectValue}
+                    onChange={e => this.updateDraft(q.codeName, e)}
+                  />
+                </div>
+              )
+            }
+          })}
+
         <Button variant="contained" fullWidth onClick={this.handleContinue}>
           {t('general.continue')}
         </Button>
