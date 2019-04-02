f5404334eb0131be04aec4823a2b38fc76795d61
Dan Atanasov <danmanm@gmail.com>
2019-04-02 16:20:13 +0300

    Setup form component with validtion

---
+++ src/components/Form.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core/styles'
+
+class Form extends Component {
+  state = {
+    errors: [],
+    showErrors: false
+  }
+
+  submit = () => {
+    // check if form is validate
+    if (!this.state.errors.length) {
+      this.props.onSubmit()
+    } else if (!this.state.showErrors) {
+      // show errors for the first time
+      this.setState({
+        showErrors: true
+      })
+    }
+  }
+
+  render() {
+    const children = React.Children.map(this.props.children, child =>
+      React.cloneElement(child, {
+        setError: (isError, field) => {
+          const { errors } = this.state
+
+          if (isError && !errors.includes(field)) {
+            this.setState({
+              errors: [...errors, field]
+            })
+            errors.push(field)
+          } else if (!isError) {
+            this.setState({
+              errors: errors.filter(item => item !== field)
+            })
+          }
+        },
+        error:
+          this.state.showErrors &&
+          this.state.errors.some(error => error === child.props.field)
+      })
+    )
+
+    return (
+      <div>
+        {children}
+        <Button
+          color="primary"
+          variant="contained"
+          fullWidth
+          onClick={this.submit}
+        >
+          {this.props.submitLabel}
+        </Button>
+      </div>
+    )
+  }
+}
+
+const styles = {
+  input: {
+    marginTop: 10,
+    marginBottom: 10
+  }
+}
+
+export default withStyles(styles)(Form)
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react'
 import TextField from '@material-ui/core/TextField'
 import { withStyles } from '@material-ui/core/styles'
+import { withTranslation } from 'react-i18next'
 
 class Input extends Component {
+  state = {
+    errorMessage: null
+  }
+
+  validate = event => {
+    const value = event ? event.target.value : null
+
+    const { t } = this.props
+
+    // if it's from onChange update value
+    if (event) {
+      this.props.onChange(this.props.field, value)
+    }
+
+    // validate
+    if (
+      this.props.required &&
+      ((!event && !this.props.value) || (event && !value))
+    ) {
+      this.props.setError(true, this.props.field)
+      this.setState({
+        errorMessage: t('validation.fieldIsRequired')
+      })
+    } else {
+      this.props.setError(false, this.props.field)
+    }
+  }
+  componentDidMount() {
+    this.validate()
+  }
   render() {
+    const { errorMessage } = this.state
+    const { error } = this.props
+
     return (
       <TextField
-        required={this.props.required || false}
         className={this.props.classes.input}
-        label={this.props.label}
+        label={`${this.props.label}${this.props.required ? ' *' : ''}`}
         value={this.props.value || ''}
-        onChange={this.props.onChange}
+        onChange={this.validate}
+        error={error}
         fullWidth
+        helperText={error && errorMessage}
       />
     )
   }
@@ -24,4 +59,4 @@ const styles = {
   }
 }
 
-export default withStyles(styles)(Input)
+export default withStyles(styles)(withTranslation()(Input))
