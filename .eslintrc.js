a6071565711e5a686640e2434a92216e6f968f5f
javierpf <javierppf@gmail.com>
2019-04-22 19:40:55 -0400

    Location UI improvements. Added prettier integration

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['react-app', 'plugin:import/errors', 'plugin:import/warnings'],
-  plugins: ['import'],
-  rules: {
-    quotes: ['error', 'single'],
-    semi: ['error', 'never']
-  }
-}
