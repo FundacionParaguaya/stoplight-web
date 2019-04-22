20c8210d5367365714a4b7188226e36e8a9749b6
gonza <gonzalo.christobal@gmail.com>
2019-04-22 16:55:37 -0400

    leave modal removed

--- src/components/NavIcons.js
+++ src/components/NavIcons.js
@@ -1,78 +1,9 @@
 import React, { Component } from 'react'
-import { withStyles, Modal, Typography, Button } from '@material-ui/core'
+import { withStyles } from '@material-ui/core'
 import { withRouter } from 'react-router-dom'
 import { theme } from '../theme'
 import { withTranslation } from 'react-i18next'
-import redExclamation from '../assets/red_exclamation.png'
-
-const modalStyles = theme => ({
-  leaveModal: {
-      width: 370,
-      height: 330,
-      backgroundColor: theme.palette.background.default,
-      outline: 'none',
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-around',
-      flexDirection: 'column',
-      padding: '25px 40px',
-  },
-  buttonContainer: {
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-around'
-  },
-  leaveModalSubtitle: {
-    textAlign: 'center'
-  }
-})
-
-const LeaveModal = withRouter(withStyles(modalStyles)((props) => {
-  const { classes } = props
-
-  const leaveSurvey = () => {
-    props.history.push('/surveys')
-  }
-  
-  return (
-    <Modal
-      open={props.open}
-      onClose={props.handleClose}
-    >
-      <div className={classes.leaveModal}>
-        <img src={redExclamation} alt="Red Exclamation" />
-        <Typography variant="h5" color="error">
-          Warning!
-        </Typography>
-        <Typography className={classes.leaveModalSubtitle} variant="subtitle1">
-          {props.subtitleModalText}
-        </Typography>
-        <div className={classes.buttonContainer}>
-          <Button
-            className={classes.button}
-            variant="outlined"
-            color="secondary"
-            onClick={leaveSurvey}
-          >
-            {props.deleteButtonText}
-          </Button>
-          <Button
-            className={classes.button}
-            variant="outlined"
-            onClick={props.handleClose}
-          >
-            {props.cancelButtonText}
-          </Button>
-        </div>
-      </div>
-    </Modal>
-  )
-}))
+import LeaveModal from '../components/LeaveModal'
 
 class NavIcons extends Component {
   state = {
@@ -80,6 +11,9 @@ class NavIcons extends Component {
   }
   handleClose = () => {
     this.setState({ showLeaveModal: false })
+  }
+  leaveSurvey = () => {
+    this.props.history.push('/surveys')
   }
   render() {
     const { classes, t } = this.props
