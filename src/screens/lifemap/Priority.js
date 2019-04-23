d1acbe1415e53f0c9b3e92658d59855fe0d408e9
Pavel Belyaev <zommerberg@abv.bg>
2019-04-23 16:27:37 +0300

    fix #265 adeed new design to priority and achievements screems

--- src/screens/lifemap/Priority.js
+++ src/screens/lifemap/Priority.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
-import { withTranslation } from 'react-i18next'
+import { withTranslation, composeInitialProps } from 'react-i18next'
 import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Form from '../../components/Form'
 import Input from '../../components/Input'
 import { updateDraft } from '../../redux/actions'
-
+import TitleBar from '../../components/TitleBar'
+import ContainerSmall from '../../components/ContainerSmall'
+import CircularProgress from '@material-ui/core/CircularProgress'
 class Priority extends Component {
   priority = this.props.currentDraft.priorities.find(
     item => item.indicator === this.props.match.params.indicator
   )
 
   state = {
+    imageStatus: 'loading',
     question: this.props.currentSurvey.surveyStoplightQuestions.find(
       indicator => indicator.codeName === this.props.match.params.indicator
     ),
@@ -19,7 +23,9 @@ class Priority extends Component {
     action: (this.priority && this.priority.action) || '',
     estimatedDate: (this.priority && this.priority.estimatedDate) || 1
   }
-
+  handleImageLoaded = () => {
+    this.setState({ imageStatus: 'loaded' })
+  }
   updateAnswer = (field, value) => {
     this.setState({
       [field]: field === 'estimatedDate' ? parseInt(value, 10) : value
@@ -64,36 +70,130 @@ class Priority extends Component {
   }
 
   render() {
-    const { t } = this.props
+    const { t, currentDraft, classes } = this.props
     const { question } = this.state
 
     return (
       <div>
-        <Typography variant="h3" gutterBottom>
-          {question.questionText}
-        </Typography>
-        <h3>{t('views.lifemap.priorities')}</h3>
-        <Form onSubmit={this.savePriority} submitLabel={t('general.save')}>
-          <Input
-            label={t('views.lifemap.whyDontYouHaveIt')}
-            value={this.state.reason}
-            field="reason"
-            onChange={this.updateAnswer}
-          />
-          <Input
-            label={t('views.lifemap.whatWillYouDoToGetIt')}
-            value={this.state.action}
-            field="action"
-            onChange={this.updateAnswer}
-          />
-          <Input
-            months
-            label={t('views.lifemap.howManyMonthsWillItTake')}
-            value={this.state.estimatedDate}
-            field="estimatedDate"
-            onChange={this.updateAnswer}
-          />
-        </Form>
+        <TitleBar
+          title={question && question.dimension}
+          extraTitleText={question && question.questionText}
+        />
+        {question !== null
+          ? currentDraft.indicatorSurveyDataList.map(ele => {
+              let url
+              let color
+              let description
+              let textColor = 'white'
+              if (question.codeName === ele.key) {
+                question.stoplightColors.forEach(elem => {
+                  if (elem.value === ele.value) {
+                    url = elem.url
+                    description = elem.description
+                    if (elem.value === 3) {
+                      color = '#89bd76'
+                    } else if (elem.value === 2) {
+                      color = '#f0cb17'
+                      textColor = 'black'
+                    } else if (elem.value === 1) {
+                      color = '#e1504d'
+                    }
+                  }
+                })
+                return (
+                  <React.Fragment>
+                    <div className={classes.imgAndDescriptionContainer}>
+                      {this.state.imageStatus === 'loading' ? (
+                        <React.Fragment>
+                          <div>
+                            {' '}
+                            <CircularProgress />
+                          </div>
+                          <img
+                            onLoad={this.handleImageLoaded}
+                            className={classes.imgClass}
+                            src={url}
+                            alt="surveyImg"
+                          />
+                        </React.Fragment>
+                      ) : (
+                        <img
+                          className={classes.imgClass}
+                          src={url}
+                          alt="surveyImg"
+                        />
+                      )}
+                      <div className={classes.answeredQuestion}>
+                        <i
+                          style={{
+                            color: 'white',
+                            backgroundColor: color,
+
+                            fontSize: 39,
+                            height: 80,
+                            width: 80,
+                            margin: 'auto',
+                            display: 'flex',
+                            borderRadius: '50%',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                          }}
+                          className="material-icons"
+                        >
+                          done
+                        </i>
+                      </div>
+                      <p
+                        className={classes.paragraphContainer}
+                        style={{ backgroundColor: color, color: textColor }}
+                      >
+                        {description}
+                      </p>
+                    </div>
+
+                    <div className={classes.pinAndPriority}>
+                      <i
+                        style={{ fontSize: '50px', color: '#77ABE9' }}
+                        class="material-icons"
+                      >
+                        assignment_returned
+                      </i>
+                      <span style={{ fontSize: '30px' }}>
+                        {t('views.lifemap.priorities')}
+                      </span>
+                    </div>
+                    <ContainerSmall>
+                      <Input
+                        months
+                        label={t('views.lifemap.howManyMonthsWillItTake')}
+                        value={this.state.estimatedDate}
+                        field="estimatedDate"
+                        onChange={this.updateAnswer}
+                      />
+                      <Form
+                        key={question.codeName}
+                        onSubmit={this.savePriority}
+                        submitLabel={t('general.save')}
+                      >
+                        <Input
+                          label={t('views.lifemap.whyDontYouHaveIt')}
+                          value={this.state.reason}
+                          field="reason"
+                          onChange={this.updateAnswer}
+                        />
+                        <Input
+                          label={t('views.lifemap.whatWillYouDoToGetIt')}
+                          value={this.state.action}
+                          field="action"
+                          onChange={this.updateAnswer}
+                        />
+                      </Form>
+                    </ContainerSmall>
+                  </React.Fragment>
+                )
+              }
+            })
+          : null}
       </div>
     )
   }
@@ -104,8 +204,45 @@ const mapStateToProps = ({ currentSurvey, currentDraft }) => ({
   currentDraft
 })
 const mapDispatchToProps = { updateDraft }
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withTranslation()(Priority))
+const styles = {
+  answeredQuestion: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)',
+    zIndex: 1
+  },
+  pinAndPriority: {
+    marginTop: '20px',
+    marginBottom: '20px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  imgClass: {
+    width: '307px'
+  },
+  paragraphContainer: {
+    margin: '0px',
+    padding: '48px 45px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '307px'
+  },
+  imgAndDescriptionContainer: {
+    position: 'relative',
+    height: '307px',
+    display: 'flex',
+    width: '614px',
+    margin: 'auto',
+    marginTop: '30px'
+  }
+}
+export default withStyles(styles)(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(withTranslation()(Priority))
+)
