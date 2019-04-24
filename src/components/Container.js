d41355b0f5e7048e56129170a13a590c126ec730
gonza <gonzalo.christobal@gmail.com>
2019-04-24 17:00:40 -0400

    grid imported

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core';
+import { withStyles, Grid } from '@material-ui/core';
 import clsx from 'clsx';
 
 function Container(props) {
