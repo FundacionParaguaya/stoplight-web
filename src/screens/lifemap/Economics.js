7484e3a74396366aa0c0bab9e3041e1e4dde2d40
Dan Atanasov <danmanm@gmail.com>
2019-04-25 15:05:41 +0300

    Make priority month required - fix #246

--- src/screens/lifemap/Economics.js
+++ src/screens/lifemap/Economics.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { updateDraft } from '../../redux/actions'
-import { withStyles } from '@material-ui/core/styles'
-import { withTranslation } from 'react-i18next'
-import TitleBar from '../../components/TitleBar'
-import Input from '../../components/Input'
-import Select from '../../components/Select'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { updateDraft } from '../../redux/actions';
+import { withStyles } from '@material-ui/core/styles';
+import { withTranslation } from 'react-i18next';
+import TitleBar from '../../components/TitleBar';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 // import Select from '@material-ui/core/Select'
-import Form from '../../components/Form'
+import Form from '../../components/Form';
 export class Economics extends Component {
   state = {
     questions: null,
     topic: ''
-  }
+  };
   handleContinue = () => {
     // validation happens here
 
-    const currentEconomicsPage = this.props.match.params.page
+    const currentEconomicsPage = this.props.match.params.page;
 
     if (
       currentEconomicsPage <
@@ -24,57 +24,59 @@ export class Economics extends Component {
     ) {
       this.props.history.push(
         `/lifemap/economics/${parseInt(currentEconomicsPage, 10) + 1}`
-      )
+      );
     } else {
-      this.props.history.push('/lifemap/begin-stoplight')
+      this.props.history.push('/lifemap/begin-stoplight');
     }
-  }
+  };
 
   setCurrentScreen() {
     const questions = this.props.currentSurvey.economicScreens
-      .questionsPerScreen[this.props.match.params.page]
+      .questionsPerScreen[this.props.match.params.page];
 
     this.setState({
       questions,
-      topic: questions.forFamily[0].topic
-    })
+      topic: questions.forFamily.length
+        ? questions.forFamily[0].topic
+        : questions.forFamilyMember[0].topic
+    });
   }
 
   updateFamilyMember = (codeName, value, question, familyName) => {
-    const { currentDraft } = this.props
+    const { currentDraft } = this.props;
     let FamilyMembersDataList = this.props.currentDraft.familyData
-      .familyMembersList
-    let update = false
+      .familyMembersList;
+    let update = false;
     //////////////// CHECK IF THE QUESTION IS ALREADY IN THE DATA LIST and if it is the set update to true and edit the answer
     FamilyMembersDataList.forEach(e => {
       if (e.firstName === familyName) {
         e.socioEconomicAnswers.forEach(e => {
           if (e.key === question.codeName) {
-            update = true
-            e.value = value
+            update = true;
+            e.value = value;
           }
-        })
+        });
       }
-    })
+    });
     if (update) {
-      let familyMembersList = FamilyMembersDataList
+      let familyMembersList = FamilyMembersDataList;
       this.props.updateDraft({
         ...currentDraft,
         familyData: {
           ...currentDraft.familyData,
           familyMembersList
         }
-      })
+      });
     } else {
-      let familyMembersList = FamilyMembersDataList
+      let familyMembersList = FamilyMembersDataList;
       FamilyMembersDataList.forEach(e => {
         if (e.firstName === familyName) {
           e.socioEconomicAnswers.push({
             key: question.codeName,
             value: value
-          })
+          });
         }
-      })
+      });
       //////////// add the question to the data list if it doesnt exist
       this.props.updateDraft({
         ...currentDraft,
@@ -82,32 +84,32 @@ export class Economics extends Component {
           ...currentDraft.familyData,
           familyMembersList
         }
-      })
+      });
     }
-  }
+  };
   setFamilyMemberName = e => {
-    console.log(e)
-  }
+    console.log(e);
+  };
 
   updateDraft = (codeName, value) => {
-    const { currentDraft } = this.props
-    let dataList = this.props.currentDraft.economicSurveyDataList
-    let update = false
+    const { currentDraft } = this.props;
+    let dataList = this.props.currentDraft.economicSurveyDataList;
+    let update = false;
     //////////////// CHECK IF THE QUESTION IS ALREADY IN THE DATA LIST and if it is the set update to true and edit the answer
     dataList.forEach(e => {
       if (e.key === codeName) {
-        update = true
-        e.value = value
+        update = true;
+        e.value = value;
       }
-    })
+    });
 
     ///////////if the question is in the data list then update the question
     if (update) {
-      let economicSurveyDataList = dataList
+      let economicSurveyDataList = dataList;
       this.props.updateDraft({
         ...currentDraft,
         economicSurveyDataList
-      })
+      });
     } else {
       //////////// add the question to the data list if it doesnt exist
       this.props.updateDraft({
@@ -119,22 +121,22 @@ export class Economics extends Component {
             value: value
           }
         ]
-      })
+      });
     }
-  }
+  };
   componentDidMount() {
-    this.setCurrentScreen()
+    this.setCurrentScreen();
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.match.params.page !== this.props.match.params.page) {
-      this.setCurrentScreen()
+      this.setCurrentScreen();
     }
   }
 
   render() {
-    const { questions, topic } = this.state
-    const { t, currentDraft, classes } = this.props
+    const { questions, topic } = this.state;
+    const { t, currentDraft, classes } = this.props;
     return (
       <div>
         <TitleBar title={topic} />
@@ -147,12 +149,12 @@ export class Economics extends Component {
 
             {questions &&
               questions.forFamily.map(question => {
-                let selectValue
+                let selectValue;
                 currentDraft.economicSurveyDataList.forEach(e => {
                   if (e.key === question.codeName) {
-                    selectValue = e.value
+                    selectValue = e.value;
                   }
-                })
+                });
                 if (question.answerType === 'select') {
                   return (
                     <Select
@@ -163,7 +165,7 @@ export class Economics extends Component {
                       field={question.codeName}
                       onChange={this.updateDraft}
                     />
-                  )
+                  );
                 } else {
                   return (
                     <Input
@@ -174,7 +176,7 @@ export class Economics extends Component {
                       field={question.codeName}
                       onChange={this.updateDraft}
                     />
-                  )
+                  );
                 }
               })}
 
@@ -196,15 +198,15 @@ export class Economics extends Component {
                             {' '}
                             {questions &&
                               questions.forFamilyMember.map(question => {
-                                let selectValue
+                                let selectValue;
 
                                 currentDraft.familyData.familyMembersList[
                                   index
                                 ].socioEconomicAnswers.forEach(ele => {
                                   if (ele.key === question.codeName) {
-                                    selectValue = ele.value
+                                    selectValue = ele.value;
                                   }
-                                })
+                                });
 
                                 if (question.answerType === 'select') {
                                   return (
@@ -223,7 +225,7 @@ export class Economics extends Component {
                                         )
                                       }
                                     />
-                                  )
+                                  );
                                 } else {
                                   return (
                                     <Input
@@ -241,12 +243,12 @@ export class Economics extends Component {
                                         )
                                       }
                                     />
-                                  )
+                                  );
                                 }
                               })}
                           </React.Fragment>
                         </React.Fragment>
-                      )
+                      );
                     }
                   }
                 )}
@@ -255,26 +257,26 @@ export class Economics extends Component {
           </React.Fragment>
         </Form>
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = ({ currentSurvey, currentDraft }) => ({
   currentSurvey,
   currentDraft
-})
+});
 
-const mapDispatchToProps = { updateDraft }
+const mapDispatchToProps = { updateDraft };
 const styles = {
   familyMemberNameLarge: {
     marginTop: '15px',
     marginBottom: '-10px',
     fontSize: '23px'
   }
-}
+};
 export default withStyles(styles)(
   connect(
     mapStateToProps,
     mapDispatchToProps
   )(withTranslation()(Economics))
-)
+);
