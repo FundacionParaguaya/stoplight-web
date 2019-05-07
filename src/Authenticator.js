b914e29c7b79f50265399a5d891f6c930c0917f5
javierpf <javierppf@gmail.com>
2019-05-07 13:04:19 -0400

    Integrated auth flow with token validation endpoint

--- src/Authenticator.js
+++ src/Authenticator.js
@@ -5,6 +5,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import { updateUser } from './redux/actions';
+import { checkSessionToken } from './api';
 
 let LoadingAuth = ({ classes }) => (
   <div className={classes.container}>
@@ -43,11 +44,16 @@ const Authenticator = props => {
   const {
     updateUser: updateUserActionDispatch,
     _persist: { rehydrated },
+    user,
     location,
     children
   } = props;
   const [authVerified, setAuthVerified] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
+
+  const redirect = to =>
+    window.location.replace(`https://${to}.povertystoplight.org/login.html`);
+
   const sid = useMemo(() => queryString.parse(location.search).sid, [
     location.search
   ]);
@@ -55,35 +61,66 @@ const Authenticator = props => {
   const env =
     useMemo(() => queryString.parse(location.search).env, [location.search]) ||
     'testing';
+
+  const { localStorageToken, localStorageEnviroment } = useMemo(() => {
+    const { token, env: envFromStorage } = user || {};
+    return { localStorageToken: token, localStorageEnviroment: envFromStorage };
+  }, [user]);
+
   useEffect(() => {
-    console.log(`Verifying login SID ${sid} rehydrated ${rehydrated}`);
-    if (!sid || !env) {
-      // Did not received expected query params. Fatal error
-      setAuthVerified(true);
-      // TODO query params are getting deleted in every redirect, in which case the next line
-      // logs user out. We'll need to further clarify this
-      // setLoggedIn(false);
-      return;
-    }
     if (!rehydrated) {
       // If it has not rehydrated yet from localStorage, it's not verified and not logged in
       setAuthVerified(false);
       setLoggedIn(false);
       return;
     }
-    // TODO DELETE THIS. Just a workaround while we don't have backend service that
-    // can validate an access token and retrieve user data from access token
-    setTimeout(() => {
-      updateUserActionDispatch({
-        username: 'Jose Perez',
-        token: sid,
-        env
-      });
-      setLoggedIn(true);
+
+    let sessionId = sid;
+    let environment = env;
+    if (!sessionId) {
+      // If there's no sid from queryParam, will try to use from localStorage
+      sessionId = localStorageToken;
+    }
+    if (!environment) {
+      // If there's no env from queryParam, will try to use from localStorage
+      environment = localStorageEnviroment;
+    }
+    if (!sessionId || !environment) {
+      // There's no sid or env to use. User no logged in
       setAuthVerified(true);
-    }, 1000);
-    // TODO we should invoke the new API endpoint and act according the response
-  }, [sid, env, rehydrated, updateUserActionDispatch]);
+      setLoggedIn(false);
+      redirect(environment);
+      return;
+    }
+    // Verifying token before logging user in
+    checkSessionToken(sessionId, environment)
+      .then(response => {
+        updateUserActionDispatch({
+          username: response.data.username,
+          token: sessionId,
+          env: environment
+        });
+        setLoggedIn(true);
+        setAuthVerified(true);
+      })
+      .catch(() => {
+        updateUserActionDispatch({
+          username: null,
+          token: null,
+          env: null
+        });
+        setLoggedIn(false);
+        setAuthVerified(true);
+        redirect(environment);
+      });
+  }, [
+    sid,
+    env,
+    rehydrated,
+    localStorageToken,
+    localStorageEnviroment,
+    updateUserActionDispatch
+  ]);
   return (
     <React.Fragment>
       {authVerified && loggedIn && <React.Fragment>{children}</React.Fragment>}
--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from 'axios';
 
 // list of environment urls
 export const url = {
@@ -6,7 +6,7 @@ export const url = {
   demo: 'https://demo.backend.povertystoplight.org',
   testing: 'https://testing.backend.povertystoplight.org',
   development: 'http://localhost:8080'
-}
+};
 
 // get a list of surveys available to the authorized used
 export const getSurveys = user =>
@@ -21,7 +21,7 @@ export const getSurveys = user =>
       query:
         'query { surveysByUser { title id minimumPriorities privacyPolicy { title  text } termsConditions{ title text }  surveyConfig { documentType {text value} gender { text value} surveyLocation { country latitude longitude} }  surveyEconomicQuestions { questionText codeName answerType topic required forFamilyMember options {text value} } surveyStoplightQuestions { questionText codeName dimension id stoplightColors { url value description } required } } }'
     })
-  })
+  });
 
 // submit a new snapshot/lifemap/draft
 export const submitDraft = (user, snapshot) =>
@@ -37,4 +37,14 @@ export const submitDraft = (user, snapshot) =>
         'mutation addSnapshot($newSnapshot: NewSnapshotDTOInput) {addSnapshot(newSnapshot: $newSnapshot)  { surveyId surveyVersionId snapshotStoplightAchievements { action indicator roadmap } snapshotStoplightPriorities { reason action indicator estimatedDate } family { familyId } user { userId  username } indicatorSurveyDataList {key value} economicSurveyDataList {key value} familyDataDTO { latitude longitude accuracy familyMemberDTOList { firstName lastName socioEconomicAnswers {key value} } } } }',
       variables: { newSnapshot: snapshot }
     })
-  })
+  });
+
+export const checkSessionToken = (token, env) =>
+  axios({
+    method: 'get',
+    url: `${url[env]}/api/v1/users/validate`,
+    headers: {
+      Authorization: `Bearer ${token}`
+      // 'Content-Type': 'application/json'
+    }
+  });
