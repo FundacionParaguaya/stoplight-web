bb54c02767c275f68f0880d885a6ea8474158b30
gonza <gonzalo.christobal@gmail.com>
2019-05-28 23:42:30 -0400

    customDocumentType added to initialValues

--- src/screens/lifemap/PrimaryParticipant.js
+++ src/screens/lifemap/PrimaryParticipant.js
@@ -349,6 +349,7 @@ export class PrimaryParticipant extends Component {
       lastName: '',
       gender: '',
       customGender: '',
+      customDocumentType: '',
       birthDate: '',
       documentType: '',
       documentNumber: '',
