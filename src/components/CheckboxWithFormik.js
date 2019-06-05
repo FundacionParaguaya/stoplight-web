c742818150ccd6f22c6d3962b9626712e31d85a5
gonza <gonzalo.christobal@gmail.com>
2019-06-05 19:11:54 -0400

    Checkbox component added

---
+++ src/components/CheckboxWithFormik.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import {
+  FormControlLabel,
+  Checkbox,
+  withStyles,
+  FormGroup,
+  FormLabel,
+  FormHelperText
+} from '@material-ui/core';
+import { get } from 'lodash';
+import { connect } from 'formik';
+import { withTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+import { pathHasError, getErrorLabelForPath } from '../utils/form-utils';
+
+const removeByIndex = (array, index) => {
+  const prior = array.slice(0, index);
+  const following = array.slice(index + 1);
+  return [...prior, ...following];
+};
+
+const CheckboxWithFormik = ({
+  rawOptions,
+  label,
+  classes,
+  name,
+  onChange,
+  formik,
+  required,
+  t
+}) => {
+  const values = get(formik.values, name) || [];
+  const error = pathHasError(name, formik.touched, formik.errors);
+  const helperText = getErrorLabelForPath(
+    name,
+    formik.touched,
+    formik.errors,
+    t
+  );
+
+  const labelProps = { required, error };
+  const handleChange = e => {
+    const index = values.indexOf(e.target.value);
+    if (index === -1) return onChange([...values, e.target.value]);
+    return onChange(removeByIndex(values, index));
+  };
+
+  return (
+    <FormGroup>
+      <FormLabel
+        className={classes.formLabel}
+        component="legend"
+        {...labelProps}
+      >
+        {label}
+      </FormLabel>
+      {rawOptions.map(option => (
+        <StyledFormControlLabel
+          control={
+            <GreenCheckbox
+              name={name}
+              onChange={e => {
+                handleChange(e);
+              }}
+              checked={values.indexOf(option.value) !== -1}
+            />
+          }
+          label={option.text}
+          key={option.value}
+          value={option.value}
+        />
+      ))}
+      {required && <FormHelperText error={error}>{helperText}</FormHelperText>}
+    </FormGroup>
+  );
+};
+
+CheckboxWithFormik.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  rawOptions: PropTypes.array.isRequired
+};
+
+const GreenCheckbox = withStyles(theme => ({
+  root: {
+    color: '#909090'
+  },
+  checked: {
+    color: theme.palette.primary.main
+  }
+}))(props => <Checkbox color="default" {...props} />);
+
+const StyledFormControlLabel = withStyles(() => ({
+  root: {
+    marginLeft: 0
+  }
+}))(props => <FormControlLabel {...props} />);
+
+const styles = {
+  formLabel: {
+    color: '#909090',
+    marginBottom: 15,
+    marginTop: 15
+  }
+};
+
+export default withTranslation()(
+  connect(withStyles(styles)(CheckboxWithFormik))
+);
