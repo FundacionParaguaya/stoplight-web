1fa27b7638e35f3afed5fa27c778061e9a38a3f6
gonza <gonzalo.christobal@gmail.com>
2019-06-14 18:50:58 -0400

    withHeader moved to /components

--- src/components/withHeader.js
+++ src/components/withHeader.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import Header from '../Header';
 import defaultTheme from '../theme';
 
 const withHeader = Component => {
-  return props => {
+  return withRouter(props => {
     return (
       <>
-        <Header />
+        <Header path={props.location.match.path} />
         <div style={{ marginTop: defaultTheme.shape.header }}>
           <Component {...props} />
         </div>
       </>
     );
-  };
+  });
 };
 
 export default withHeader;
