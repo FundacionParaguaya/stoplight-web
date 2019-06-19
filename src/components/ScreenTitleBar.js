7f205fd6e6ea8e7f3bc7d606e8759be7a1887c37
gonza <gonzalo.christobal@gmail.com>
2019-06-18 22:00:33 -0400

    Beta Badge added

--- src/components/ScreenTitleBar.js
+++ src/components/ScreenTitleBar.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { Typography, withStyles } from '@material-ui/core';
+import { Typography, withStyles, Badge } from '@material-ui/core';
 
-const ScreenTitleBar = ({ classes, title, subtitle }) => {
+const ScreenTitleBar = ({ classes, title, subtitle, betaBadge }) => {
   return (
     <div className={classes.container}>
-      <Typography variant="h4">{title}</Typography>
+      {betaBadge && (
+        <StyledBadge badgeContent="Beta!" className={classes.badge}>
+          <Typography variant="h4">{title}</Typography>
+        </StyledBadge>
+      )}
+      {!betaBadge && <Typography variant="h4">{title}</Typography>}
       <Typography variant="h6">{subtitle || ''}</Typography>
     </div>
   );
 };
+
+const StyledBadge = withStyles(theme => ({
+  badge: {
+    color: '#fff',
+    backgroundColor: theme.palette.secondary.main,
+    fontSize: '10px!important',
+    borderRadius: 2,
+    fontFamily: 'Poppins',
+    padding: '0px 5px',
+    paddingTop: 1,
+    top: 10,
+    right: -10
+  }
+}))(props => <Badge {...props} />);
 
 const styles = {
   container: {
