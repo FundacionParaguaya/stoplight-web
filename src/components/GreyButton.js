33567413b5f722fb76933e4c68d6dfd9ba818a37
gonza <gonzalo.christobal@gmail.com>
2019-07-10 11:13:41 -0400

    GreyButton created

---
+++ src/components/GreyButton.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { withStyles, Button } from '@material-ui/core';
+
+const GreyButton = ({ children, className, classes, ...props }) => (
+  <Button className={`${classes.button} ${className}`} {...props}>
+    Show more
+  </Button>
+);
+
+const styles = theme => ({
+  button: {
+    textDecoration: 'none',
+    fontSize: 14,
+    color: theme.palette.grey.middle,
+    padding: `${theme.spacing(1.75)}px ${theme.spacing(5)}px`,
+    lineHeight: 1,
+    textTransform: 'capitalize',
+    backgroundColor: '#FAFBFC',
+    borderRadius: 2,
+    border: '0.5px solid #D8D2D2',
+    '&:hover': {
+      textDecoration: 'none'
+    },
+    '&::after, &::before': {
+      display: 'none'
+    }
+  }
+});
+
+export default withStyles(styles)(GreyButton);
