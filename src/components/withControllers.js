da242eb6d6d6aa8362988a62166d0307f4aeaa1c
gonza <gonzalo.christobal@gmail.com>
2019-07-12 11:32:04 -0400

    Controllers and Show More button HOC

---
+++ src/components/withControllers.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import {
+  withStyles,
+  Typography,
+  Box,
+  CircularProgress
+} from '@material-ui/core';
+import { useTheme } from '@material-ui/styles';
+import GreyButton from './GreyButton';
+import Controllers from './Controllers';
+import { BAR } from '../utils/types';
+
+const styles = theme => ({
+  innerContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%'
+  },
+  loadingContainer: {
+    width: '100%',
+    minHeight: 495,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: theme.palette.grey.light
+  },
+  buttonContainer: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center'
+  }
+});
+
+function withControllers(WrappedComponent) {
+  return withStyles(styles)(({ loading, classes, ...props }) => {
+    const [type, setType] = useState(BAR);
+    const [count, setCount] = useState(10);
+    const theme = useTheme();
+
+    return (
+      <div>
+        <div className={classes.innerContainer}>
+          <Typography variant="h5">Dimensions</Typography>
+          <Controllers type={type} setIndicatorsType={setType} />
+        </div>
+        <Box mt={1} />
+        {loading && (
+          <div className={classes.loadingContainer}>
+            <CircularProgress
+              size={50}
+              thickness={2}
+              style={{ color: theme.palette.grey.main }}
+            />
+          </div>
+        )}
+        {!loading && props.data.length > 0 && (
+          <>
+            <WrappedComponent type={type} data={props.data.slice(0, count)} />
+            {props.data.length > 10 && (
+              <>
+                <Box mt={4} />
+                <div className={classes.buttonContainer}>
+                  <GreyButton
+                    onClick={() => setCount(prev => prev + 10)}
+                    disabled={props.data.length < count}
+                  >
+                    Show more
+                  </GreyButton>
+                </div>
+              </>
+            )}
+          </>
+        )}
+      </div>
+    );
+  });
+}
+
+export default withControllers;
