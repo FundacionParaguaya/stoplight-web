863203e0d4aa89b05686f693efd00cfb63e5d693
gonza <gonzalo.christobal@gmail.com>
2019-08-05 19:21:12 -0400

    Add role consts

---
+++ src/utils/role-utils.js
@@ -0,0 +1,7 @@
+export const ROLE_ROOT = 'ROLE_ROOT';
+export const ROLE_HUB_ADMIN = 'ROLE_HUB_ADMIN';
+export const ROLE_PS_TEAM = 'ROLE_PS_TEAM';
+export const ROLE_APP_ADMIN = 'ROLE_APP_ADMIN';
+export const ROLE_SURVEY_USER = 'ROLE_SURVEY_USER';
+
+// TODO @gonzarodriguezt find a better way to handle roles
