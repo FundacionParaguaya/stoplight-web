c8e5b36c48f41539ce5cf2006d8b700982f0a694
Gabriela Gaona <gabriela@codium.com.py>
2020-01-15 14:11:58 -0300

    css adjustments

--- src/components/FamilyPriorities.js
+++ src/components/FamilyPriorities.js
@@ -9,6 +9,7 @@ import Container from '../components/Container';
 import { withSnackbar } from 'notistack';
 import * as _ from 'lodash';
 import iconPriority from '../assets/icon_priority.png';
+import { Accordion, AccordionItem } from 'react-sanfona';
 
 const FamilyPriorities = ({ classes, user, t, i18n: { language } }) => {
   return (
@@ -27,11 +28,82 @@ const FamilyPriorities = ({ classes, user, t, i18n: { language } }) => {
       <Container className={classes.basicInfoText} variant="fluid">
         <Typography variant="h5">Priorities · 5</Typography>
       </Container>
+
+      <div className={classes.prioritiesContainer}>
+        <div className={classes.columnHeaderContainer}>
+          <Typography className={classes.labelRows} variant="subtitle1">
+            Indicator
+          </Typography>
+
+          <Typography className={classes.labelRows} variant="subtitle1">
+            Review Date
+          </Typography>
+        </div>
+        <Accordion className={classes.priorityTable}>
+          {[1, 2, 3, 4, 5].map(item => {
+            return (
+              <AccordionItem
+                className={classes.priorityTitle}
+                title={`Item ${item}`}
+                expanded={item === 1}
+              >
+                <div className={classes.priorityContent}>
+                  {`Item ${item} content`}
+                </div>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+      </div>
     </div>
   );
 };
 
 const styles = theme => ({
+  priorityContent: {},
+
+  priorityTitle: {
+    paddingRight: '2rem',
+    paddingLeft: '2rem',
+    paddingTop: '1rem',
+    paddingBotton: '1rem',
+    '&:nth-child(2n - 1)': {
+      backgroundColor: '#F3F4F6'
+    }
+  },
+  priorityTable: {
+    width: '100%',
+    mixBlendMode: 'normal',
+    opacity: '0.5',
+    paddingBotton: '2rem'
+  },
+  labelRows: {
+    marginRight: 10,
+    marginLeft: 10,
+    fontSize: 16,
+    height: 20,
+    color: '#6A6A6A'
+  },
+  columnHeaderContainer: {
+    height: 37,
+    width: '100%',
+    opacity: 1,
+    backgroundColor: '#DCDEE3',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  prioritiesContainer: {
+    backgroundColor: '#fff',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    paddingTop: '2rem',
+    paddingRight: '12%',
+    paddingLeft: '12%',
+    paddingBottom: '2rem'
+  },
   basicInfo: {
     backgroundColor: theme.palette.background.default,
     display: 'flex',
