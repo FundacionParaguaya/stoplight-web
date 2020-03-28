538a070948225b14f6dc0d9acb4a92249febf92a
JDelPuerto <jdelpuerto@codium.com.py>
2020-03-28 17:16:32 -0400

    Added redireaction to a new view

--- src/screens/FamilyProfile.js
+++ src/screens/FamilyProfile.js
@@ -24,7 +24,6 @@ import Divider from '../components/Divider';
 import AllSurveyIndicators from '../components/summary/AllSurveyIndicators';
 import { getDateFormatByLocale } from '../utils/date-utils';
 import moment from 'moment';
-import { getPlatform } from '../utils/role-utils';
 import FacilitatorFilter from '../components/FacilitatorFilter';
 import Grid from '@material-ui/core/Grid';
 import { ROLES_NAMES } from '../utils/role-utils';
@@ -64,10 +63,8 @@ const FamilyProfile = ({
     { label: t('views.familyProfile.profile'), link: `/family/${familyId}` }
   ];
 
-  const goToFamilyPsp = e => {
-    window.location.replace(
-      `${getPlatform(user.env)}/#families/${familyId}/snapshots`
-    );
+  const goToFamilyDetails = e => {
+    history.push(`/detail/${familyId}`);
   };
 
   const changeFacilitator = () => {
@@ -514,7 +511,7 @@ const FamilyProfile = ({
                 variant="subtitle1"
                 className={classes.labelGreenRight}
               >
-                <Link href="#" onClick={goToFamilyPsp}>
+                <Link onClick={goToFamilyDetails}>
                   {t('views.familyProfile.viewLifeMap')}
                 </Link>
               </Typography>
