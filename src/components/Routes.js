42c21d6fc8dfa698eb72b4be5496d8462ad83d4e
JDelPuerto <jdelpuerto@codium.com.py>
2020-05-21 16:20:15 -0400

    Refactor:  Page not found component added

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Route, Switch, Link } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
-import { makeStyles } from '@material-ui/core/styles';
+import { Route, Switch } from 'react-router-dom';
 import { checkAccess } from '../utils/role-utils';
 import Surveys from '../screens/SurveysWithDrafts';
 import SurveyList from '../screens/Surveys';
@@ -14,88 +12,9 @@ import LifemapDetails from '../screens/LifemapDetails';
 import SelectIndicatorPriority from '../screens/priorities/SelectIndicatorPriority';
 import Families from '../screens/Families';
 import Dashboard from '../screens/Dashboard';
-import Typography from '@material-ui/core/Typography';
-import interrogation from '../assets/interrogation.png';
-import { useTranslation } from 'react-i18next';
-import { ROLES_NAMES } from '../utils/role-utils';
+import PageNotFound from '../screens/PageNotFound';
 
 const Routes = ({ user }) => {
-  const pageNotFoundStyles = makeStyles(theme => ({
-    container: {
-      display: 'flex',
-      height: '100vh',
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    imageContainer: {
-      display: 'flex',
-      width: '50%'
-    },
-    img: {
-      maxWidth: '80%',
-      maxHeight: 800,
-      margin: 'auto'
-    },
-    textContainer: {
-      width: '50%',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'left',
-      padding: 20
-    },
-    text: {
-      color: theme.palette.grey.middle
-    },
-    codeText: {
-      fontSize: 120,
-      fontWeight: 600,
-      color: theme.palette.grey.main,
-      height: 90
-    },
-    linkText: {
-      fontWeight: 600,
-      position: 'relative',
-      marginTop: 20,
-      textDecoration: 'none',
-      fontSize: 16,
-      color: theme.palette.primary.dark
-    }
-  }));
-
-  const { t } = useTranslation();
-
-  const classes = pageNotFoundStyles();
-
-  const redirectionPath = ({ role }) => {
-    if (role === ROLES_NAMES.ROLE_SURVEY_TAKER) {
-      return 'surveys';
-    } else return 'dashboard';
-  };
-
-  let PageNotFound = ({ user }) => (
-    <div className={classes.container}>
-      <div className={classes.imageContainer}>
-        <img src={interrogation} alt="interrogation" className={classes.img} />
-      </div>
-      <div className={classes.textContainer}>
-        <Typography variant="h4" className={classes.codeText}>
-          {'404'}
-        </Typography>
-        <Typography variant="h5" className={classes.text}>
-          {t('views.404.missing')}
-        </Typography>
-        <Typography variant="h6" className={classes.text}>
-          {t('views.404.pageNotFound')}
-        </Typography>
-        <Link to={redirectionPath(user)} className={classes.linkText}>
-          {`${t('views.404.toHomePage')} >`}
-        </Link>
-      </div>
-    </div>
-  );
-
-  PageNotFound = withStyles(pageNotFoundStyles)(PageNotFound);
-
   return (
     <Switch>
       {checkAccess(user, 'surveys') && (
