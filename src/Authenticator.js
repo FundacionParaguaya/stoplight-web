bece1f4e292f3aa0f0d0c1e0a5a5a82c49e2d970
Guillecaba <guillermocaba95@gmail.com>
2020-06-16 17:12:13 -0400

    fix: refactor get interactive_help

--- src/Authenticator.js
+++ src/Authenticator.js
@@ -126,9 +126,9 @@ const Authenticator = props => {
           hub: response.data.application,
           organization: response.data.organization,
           name: response.data.name,
-          interative_help: response.data.application
-            ? !!response.data.application.interactiveHelp
-            : false
+          interative_help:
+            !!response.data.application &&
+            !!response.data.application.interactiveHelp
         });
         setLoggedIn(true);
         setAuthVerified(true);
