174e258df0033f88f2eae289678eaaefb5fb75ce
JDelPuerto <jdelpuerto@codium.com.py>
2020-09-17 15:20:35 -0400

    Added text filter for surveys tab

--- src/api.js
+++ src/api.js
@@ -119,7 +119,7 @@ export const getSurveysByUser = user =>
     })
   });
 
-export const surveysByUserPaginated = (user, page) =>
+export const surveysByUserPaginated = (user, filter, page) =>
   axios({
     method: 'post',
     url: `${url[user.env]}/graphql`,
@@ -128,8 +128,9 @@ export const surveysByUserPaginated = (user, page) =>
     },
     data: JSON.stringify({
       query:
-        'query surveysByUserPaginated($page: Int, $sortBy: String, $sortDirection: String) { surveysByUserPaginated(page:$page, sortBy:$sortBy, sortDirection:$sortDirection ){content {id,title, indicatorsCount, createdAt, organizations{id,name}, applications{id,name}} totalPages totalElements }}',
+        'query surveysByUserPaginated($filter: String, $page: Int, $sortBy: String, $sortDirection: String) { surveysByUserPaginated(filter:$filter, page:$page, sortBy:$sortBy, sortDirection:$sortDirection ){content {id,title, indicatorsCount, createdAt, organizations{id,name}, applications{id,name}} totalPages totalElements }}',
       variables: {
+        filter: filter,
         page: page,
         sortBy: '',
         sortDirection: ''
