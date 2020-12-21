d2c99b5a17ff8ef442f23d7ab56d5c484f0ae4af
Tomas Silgueira <tsilgueira@codium.com.py>
2020-12-21 13:38:49 -0300

    TSilgueira 1450 Adjust search box landscape responsive
    Signed-off-by: Tomas Silgueira <tsilgueira@codium.com.py>

--- src/components/FamilyFilter.js
+++ src/components/FamilyFilter.js
@@ -181,8 +181,8 @@ const FamilyFilter = ({
         )}
         <Grid
           item
-          md={5}
-          sm={5}
+          md={7}
+          sm={7}
           xs={12}
           container
           className={classes.container}
