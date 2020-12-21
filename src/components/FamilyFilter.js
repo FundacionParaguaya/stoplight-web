b8e281c3e9adf7c6c6c5800fcdecfe2f3ecfeb69
Tomas Silgueira <tsilgueira@codium.com.py>
2020-12-21 16:38:34 -0300

    Fix facilitators field for org's
    Signed-off-by: Tomas Silgueira <tsilgueira@codium.com.py>

--- src/components/FamilyFilter.js
+++ src/components/FamilyFilter.js
@@ -214,7 +214,7 @@ const FamilyFilter = ({
           </div>
         </Grid>
         {showFalicitatorFilters(user) && (
-          <Grid item md={7} sm={7} xs={12}>
+          <Grid item md={5} sm={5} xs={12}>
             <FacilitatorFilter
               data={facilitatorsData}
               organizations={organizationsData}
