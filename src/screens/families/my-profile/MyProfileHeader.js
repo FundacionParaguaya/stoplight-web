4aeba91a8a53f10daf0671f8b383a29172c5cfc1
Jorge Del Puerto <jorgedelpuerto26@gmail.com>
2021-03-11 15:35:33 -0300

    Added  profile picture feature

--- src/screens/families/my-profile/MyProfileHeader.js
+++ src/screens/families/my-profile/MyProfileHeader.js
@@ -1,11 +1,15 @@
+import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import React from 'react';
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import chooseLifeMap from '../../../assets/family.png';
+import leftColors from '../../../assets/left.png';
+import defaultImage from '../../../assets/profile_placeholder.png';
+import rightColors from '../../../assets/right.png';
 import Container from '../../../components/Container';
 import { checkAccessToProjects } from '../../../utils/role-utils';
+import UploadImageModal from './UploadImageModal';
 
 const useStyles = makeStyles(theme => ({
   titleContainer: {
@@ -27,22 +31,20 @@ const useStyles = makeStyles(theme => ({
     }
   },
   image: {
-    display: 'block',
-    width: '40%',
-    right: 30,
-    position: 'absolute',
-    top: -51,
+    maxWidth: 200,
+    maxHeight: 180,
+    borderRadius: '50%',
+    marginRight: 5,
     [theme.breakpoints.down('md')]: {
-      width: '50%',
       minHeight: 155
-    },
-    [theme.breakpoints.down('sm')]: {
-      width: '60%',
-      minHeight: 155
-    },
-    [theme.breakpoints.down('xs')]: {
-      display: 'none'
     }
+  },
+  icon: {
+    position: 'absolute',
+    fontSize: 30,
+    top: 60,
+    right: '44%',
+    color: 'white'
   },
   container: {
     display: 'flex',
@@ -56,6 +58,29 @@ const useStyles = makeStyles(theme => ({
   },
   subtitle: {
     marginRight: theme.spacing(1)
+  },
+  imagesContainer: {
+    display: 'flex',
+    paddingBottom: 15,
+    justifyContent: 'flex-end',
+    [theme.breakpoints.down('sm')]: {
+      justifyContent: 'space-around'
+    }
+  },
+  leftImage: {
+    maxWidth: 115,
+    marginRight: 5,
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: 80,
+      height: 160
+    }
+  },
+  rightImage: {
+    maxWidth: 100,
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: 70,
+      height: 150
+    }
   }
 }));
 
@@ -63,8 +88,23 @@ const MyProfileHeader = ({ family, user }) => {
   const classes = useStyles();
   const { t } = useTranslation();
 
+  const [openModal, setOpenModal] = useState(false);
+
+  const toggleModal = reloadPage => {
+    setOpenModal(!openModal);
+    if (reloadPage) {
+      window.location.reload();
+    }
+  };
+
   return (
     <Container variant="stretch">
+      <UploadImageModal
+        open={openModal}
+        toggleModal={toggleModal}
+        familyId={family.familyId}
+        user={user}
+      />
       <Grid container className={classes.titleContainer}>
         <Grid item md={4} sm={6} xs={12} className={classes.familyInfo}>
           <Typography variant="h4">{family.name}</Typography>
@@ -88,12 +128,23 @@ const MyProfileHeader = ({ family, user }) => {
             </div>
           )}
         </Grid>
-        <Grid item md={4} sm={5}>
-          <img
-            src={chooseLifeMap}
-            alt="Choose Life Map"
-            className={classes.image}
-          />
+        <Grid item md={8} sm={6} xs={12} className={classes.imagesContainer}>
+          <img src={leftColors} alt="left" className={classes.leftImage} />
+          <div style={{ position: 'relative' }}>
+            <img
+              src={family.profilePictureUrl || defaultImage}
+              alt="Profile"
+              className={classes.image}
+              onClick={() => setOpenModal(true)}
+            />
+            {!family.profilePictureUrl && (
+              <AddAPhotoIcon
+                onClick={() => setOpenModal(true)}
+                className={classes.icon}
+              />
+            )}
+          </div>
+          <img src={rightColors} alt="right" className={classes.rightImage} />
         </Grid>
       </Grid>
     </Container>
