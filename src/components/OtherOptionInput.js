986971d11ac5b7a78c5efba3bd0296d39d46d1ca
Jorge Del Puerto <jorgedelpuerto26@gmail.com>
2021-05-11 16:48:52 -0400

    Added generic other option input for forms

---
+++ src/components/OtherOptionInput.js
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+import { connect } from 'formik';
+import { get } from 'lodash';
+
+const getOtherOption = options => {
+  if (!options.some(e => e.otherOption)) {
+    return null;
+  }
+
+  return options.filter(e => e.otherOption)[0].value;
+};
+
+const OtherOptionInput = ({
+  dep,
+  fieldOptions,
+  children,
+  target,
+  cleanUp,
+  formik,
+  isMultiValue
+}) => {
+  const otherOption = getOtherOption(fieldOptions);
+
+  if (!isMultiValue) {
+    const value = formik.values[dep];
+
+    if (otherOption !== value && !!get(formik.values, target)) {
+      cleanUp(value);
+    }
+
+    if (otherOption && value) {
+      return children(otherOption, value);
+    }
+
+    return null;
+  } else {
+    const values = formik.values[dep] || [];
+
+    if (!values.find(v => v === otherOption) && !!get(formik.values, target)) {
+      cleanUp();
+    } else {
+      if (otherOption && !!values.find(v => v === otherOption)) {
+        return children(
+          otherOption,
+          values.find(v => v === otherOption)
+        );
+      }
+    }
+
+    return null;
+  }
+};
+
+OtherOptionInput.propTypes = {
+  dep: PropTypes.string.isRequired,
+  fieldOptions: PropTypes.array.isRequired,
+  children: PropTypes.func.isRequired
+};
+
+export default connect(OtherOptionInput);
