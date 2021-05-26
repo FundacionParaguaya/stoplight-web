406568576d39476ecfb6906338e06e7fb67deafe
Jorge Del Puerto <jorgedelpuerto26@gmail.com>
2021-05-26 12:40:51 -0400

    Hide intercom  popup from family users

--- src/components/Intercom.js
+++ src/components/Intercom.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Intercom from 'react-intercom';
 import { connect } from 'react-redux';
+import { ROLES_NAMES } from '../utils/role-utils';
 
 const InitializedIntercom = ({ user }) => (
-  <Intercom
-    appID="msjjl81s"
-    {...{
-      name: !!user && !!user.username ? ' ' + user.username : '',
-      email: !!user && !!user.email ? ' ' + user.email : null,
-      created_at: !!user && !!user.name && new Date().getTime()
-    }}
-  />
+  <React.Fragment>
+    {!!user && user.role !== ROLES_NAMES.ROLE_FAMILY_USER && (
+      <Intercom
+        appID="msjjl81s"
+        {...{
+          name: !!user && !!user.username ? ' ' + user.username : '',
+          email: !!user && !!user.email ? ' ' + user.email : null,
+          created_at: !!user && !!user.name && new Date().getTime()
+        }}
+      />
+    )}
+  </React.Fragment>
 );
 
 const mapStateToProps = ({ user }) => ({ user });
