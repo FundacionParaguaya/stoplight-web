f74e414c66c2e1ebb82c1e06a3628dc21d7d8346
Jorge Del Puerto <jorgedelpuerto26@gmail.com>
2021-06-30 13:51:55 -0400

    Added proptypes and UX improvement

--- src/components/CheckboxInput.js
+++ src/components/CheckboxInput.js
@@ -1,13 +1,25 @@
+import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import GreenCheckbox from './GreenCheckbox';
 
 const CheckboxInput = ({ label, onChange, checked }) => (
-  <Grid container alignItems="center">
-    <GreenCheckbox onChange={e => onChange()} checked={checked} />
+  <Grid
+    container
+    alignItems="center"
+    style={{ cursor: 'pointer' }}
+    onClick={() => onChange()}
+  >
+    <GreenCheckbox onChange={() => {}} checked={checked} />
     <Typography variant="subtitle2">{label}</Typography>
   </Grid>
 );
+
+CheckboxInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  checked: PropTypes.bool.isRequired
+};
 
 export default CheckboxInput;
