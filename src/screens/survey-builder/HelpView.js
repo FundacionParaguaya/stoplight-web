ae5bb43e529ec53d7a23f93cf4d4acb05dccb751
Jorge Del Puerto <jorgedelpuerto26@gmail.com>
2021-07-07 16:33:13 -0400

    Fixed loading animation positioning

--- src/screens/survey-builder/HelpView.js
+++ src/screens/survey-builder/HelpView.js
@@ -14,7 +14,6 @@ import SearchText from './SearchText';
 const useStyles = makeStyles(theme => ({
   container: {
     backgroundColor: theme.palette.background.paper,
-    padding: '2rem',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
@@ -27,7 +26,12 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    position: 'fixed',
+    right: 0,
+    bottom: 0,
+    top: 0,
+    left: 0
   },
   title: {
     marginBottom: '1rem'
@@ -75,11 +79,6 @@ const HelpView = ({ user }) => {
 
   return (
     <div className={classes.container}>
-      {loading && (
-        <div className={classes.loadingContainer}>
-          <CircularProgress />
-        </div>
-      )}
       <Typography variant="h5" className={classes.title}>
         {t('views.support.collections.surveyBuilderDescription')}
       </Typography>
@@ -87,7 +86,13 @@ const HelpView = ({ user }) => {
         label={t('views.support.search')}
         onKeyDown={e => onChangeFilterText(e)}
       />
-      <ArticlesList articles={articles} handleGoArticle={handleGoArticle} />
+      {loading ? (
+        <div className={classes.loadingContainer}>
+          <CircularProgress />
+        </div>
+      ) : (
+        <ArticlesList articles={articles} handleGoArticle={handleGoArticle} />
+      )}
     </div>
   );
 };
