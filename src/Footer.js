16c3f17c887c56cffb7a021a718eebbd13264acc
Diego Torres <diego@povertystoplight.org>
2021-07-20 20:48:56 -0400

    [release] Release v1.46.0
    Signed-off-by: Diego Torres <diego@povertystoplight.org>

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ function Footer(props) {
   return (
     <div className={classes.footer}>
       <Typography className={classes.footerText}>
-        ©Copyright 2020 - Stoplight Platform 1.45.0
+        ©Copyright 2020 - Stoplight Platform 1.46.0
       </Typography>
     </div>
   );
